40fc7ba381c5a5f0e88b7076fe0f6669287ce4fc
Juan Pedro Mellado <mellado@gmx.de>
2017-04-27 18:37:26 +0200

    Adding new implementation of opr_partial routines to pressure boussinesq.

--- mappings/fi_pressure_boussinesq.f90
+++ mappings/fi_pressure_boussinesq.f90
@@ -11,9 +11,9 @@
 SUBROUTINE FI_PRESSURE_BOUSSINESQ(q,s, p, tmp1,tmp2,tmp3, wrk1d,wrk2d,wrk3d)
 
   USE DNS_GLOBAL, ONLY : g
-  USE DNS_GLOBAL, ONLY : imax,jmax,kmax, isize_wrk1d
+  USE DNS_GLOBAL, ONLY : imax,jmax,kmax, isize_wrk1d, imode_eqns
+  USE DNS_GLOBAL, ONLY : rbackground
   USE DNS_GLOBAL, ONLY : visc
-  USE DNS_GLOBAL, ONLY : imode_fdm, iunify,iunifx,iunifz, i1bc,j1bc,k1bc
 
 IMPLICIT NONE
 
@@ -27,11 +27,11 @@ IMPLICIT NONE
   TREAL, DIMENSION(imax,kmax,2),      INTENT(INOUT) :: wrk2d
 
 ! -----------------------------------------------------------------------
-  TINTEGER i, k
+  TINTEGER i, k, bcs(2,2)
 
-  TREAL dx(1), dy(1), dz(1) ! To use old wrappers to calculate derivatives
-  
 ! #######################################################################
+  bcs  = 0 ! Boundary conditions for derivative operator set to biased, non-zero
+
   p    = C_0_R
   tmp3 = C_0_R
   
@@ -40,52 +40,49 @@ IMPLICIT NONE
 ! #######################################################################
 ! Calculate forcing term Ox
 ! #######################################################################
-  CALL PARTIAL_ZZ(i1, iunifz, imode_fdm, imax,jmax,kmax, k1bc,&
-       dz, q(1,1,1,1), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_Z(OPR_P2_P1, imax,jmax,kmax, bcs, g(3), q(1,1,1,1),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,1) = tmp3(:,:,:,1) + tmp2 *visc - q(:,:,:,3) *tmp1
 
-  CALL PARTIAL_YY(i1, iunify,     imode_fdm, imax,jmax,kmax, j1bc,&
-       dy, q(1,1,1,1), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_Y(OPR_P2_P1, imax,jmax,kmax, bcs, g(2), q(1,1,1,1),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,1) = tmp3(:,:,:,1) + tmp2 *visc - q(:,:,:,2) *tmp1
 
-  CALL PARTIAL_XX(i1, iunifx, imode_fdm, imax,jmax,kmax, i1bc,&
-       dx, q(1,1,1,1), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_X(OPR_P2_P1, imax,jmax,kmax, bcs, g(1), q(1,1,1,1),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,1) = tmp3(:,:,:,1) + tmp2 *visc - q(:,:,:,1) *tmp1
 
-  CALL PARTIAL_X(imode_fdm, imax,jmax,kmax, i1bc, dx, tmp3(1,1,1,1), tmp1, i0,i0, wrk1d,wrk2d,wrk3d)
+  IF ( imode_eqns .EQ. DNS_EQNS_ANELASTIC ) THEN
+     CALL THERMO_ANELASTIC_WEIGHT_INPLACE(imax,jmax,kmax, rbackground, tmp3(1,1,1,1))
+  ENDIF
+  CALL OPR_PARTIAL_X(OPR_P1, imax,jmax,kmax, bcs, g(1), tmp3(1,1,1,1),tmp1, wrk3d, wrk2d,wrk3d)
   p = p + tmp1
 
 ! #######################################################################
 ! Calculate forcing term Oz
 ! #######################################################################
-  CALL PARTIAL_ZZ(i1, iunifz, imode_fdm, imax,jmax,kmax, k1bc,&
-       dz, q(1,1,1,3), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_Z(OPR_P2_P1, imax,jmax,kmax, bcs, g(3), q(1,1,1,3),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,3) = tmp3(:,:,:,3) + tmp2 *visc - q(:,:,:,3) *tmp1
 
-  CALL PARTIAL_YY(i1, iunify,     imode_fdm, imax,jmax,kmax, j1bc,&
-       dy, q(1,1,1,3), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_Y(OPR_P2_P1, imax,jmax,kmax, bcs, g(2), q(1,1,1,3),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,3) = tmp3(:,:,:,3) + tmp2 *visc - q(:,:,:,2) *tmp1
 
-  CALL PARTIAL_XX(i1, iunifx, imode_fdm, imax,jmax,kmax, i1bc,&
-       dx, q(1,1,1,3), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_X(OPR_P2_P1, imax,jmax,kmax, bcs, g(1), q(1,1,1,3),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,3) = tmp3(:,:,:,3) + tmp2 *visc - q(:,:,:,1) *tmp1
 
-  CALL PARTIAL_Z(imode_fdm, imax,jmax,kmax, k1bc, dz, tmp3(1,1,1,3), tmp1, i0,i0, wrk1d,wrk2d,wrk3d)
+  IF ( imode_eqns .EQ. DNS_EQNS_ANELASTIC ) THEN
+     CALL THERMO_ANELASTIC_WEIGHT_INPLACE(imax,jmax,kmax, rbackground, tmp3(1,1,1,3))
+  ENDIF
+  CALL OPR_PARTIAL_Z(OPR_P1, imax,jmax,kmax, bcs, g(3), tmp3(1,1,1,3),tmp1, wrk3d, wrk2d,wrk3d)
   p = p + tmp1
 
 ! #######################################################################
 ! Calculate forcing term Oy
 ! #######################################################################
-  CALL PARTIAL_ZZ(i1, iunifz, imode_fdm, imax,jmax,kmax, k1bc,&
-       dz, q(1,1,1,2), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_Z(OPR_P2_P1, imax,jmax,kmax, bcs, g(3), q(1,1,1,2),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,2) = tmp3(:,:,:,2) + tmp2 *visc - q(:,:,:,3) *tmp1
 
-  CALL PARTIAL_YY(i1, iunify,     imode_fdm, imax,jmax,kmax, j1bc,&
-       dy, q(1,1,1,2), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_Y(OPR_P2_P1, imax,jmax,kmax, bcs, g(2), q(1,1,1,2),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,2) = tmp3(:,:,:,2) + tmp2 *visc - q(:,:,:,2) *tmp1
 
-  CALL PARTIAL_XX(i1, iunifx, imode_fdm, imax,jmax,kmax, i1bc,&
-       dx, q(1,1,1,2), tmp2, i0,i0, i0,i0, tmp1, wrk1d,wrk2d,wrk3d)
+  CALL OPR_PARTIAL_X(OPR_P2_P1, imax,jmax,kmax, bcs, g(1), q(1,1,1,2),tmp2, tmp1, wrk2d,wrk3d)
   tmp3(:,:,:,2) = tmp3(:,:,:,2) + tmp2 *visc - q(:,:,:,1) *tmp1
 
 ! Neumann BCs top and bottom
@@ -93,8 +90,13 @@ IMPLICIT NONE
      wrk2d(i,k,1) = tmp3(i,1   ,k,2)
      wrk2d(i,k,2) = tmp3(i,jmax,k,2)
   ENDDO; ENDDO
-  
-  CALL PARTIAL_Y(imode_fdm, imax,jmax,kmax, j1bc, dy, tmp3(1,1,1,2), tmp1, i0,i0, wrk1d,tmp2,wrk3d)
+
+  IF ( imode_eqns .EQ. DNS_EQNS_ANELASTIC ) THEN
+     wrk2d(:,:,1) = wrk2d(:,:,1) *rbackground(1)
+     wrk2d(:,:,2) = wrk2d(:,:,2) *rbackground(g(2)%size)
+     CALL THERMO_ANELASTIC_WEIGHT_INPLACE(imax,jmax,kmax, rbackground, tmp3(1,1,1,2))
+  ENDIF  
+  CALL OPR_PARTIAL_Y(OPR_P1, imax,jmax,kmax, bcs, g(2), tmp3(1,1,1,2),tmp1, wrk3d, wrk2d,wrk3d)
   p = p + tmp1
 
 ! #######################################################################
