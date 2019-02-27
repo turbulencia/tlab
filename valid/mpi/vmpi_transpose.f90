29a62b8efbed4f5b183643bcc61363807d8a86f8
zrick <cedrick@posteo.de>
2019-02-27 23:58:03 +0100

    vmpi_tranpose.f90 modified to measure performance

--- valid/mpi/vmpi_transpose.f90
+++ valid/mpi/vmpi_transpose.f90
@@ -15,15 +15,11 @@ MODULE DNS_MPI
   IMPLICIT NONE
   SAVE
   
-  TINTEGER, PARAMETER :: imax = 84   ! number of grid points per task
-  TINTEGER, PARAMETER :: jmax = 480
-  TINTEGER, PARAMETER :: kmax = 56
+  INTEGER(KIND=4) :: imax_total,jmax_total,kmax_total = 84   ! number of grid points per task
+  INTEGER(KIND=4) :: imax,jmax,kmax
 
-  INTEGER, PARAMETER :: ims_npro_i = 64 ! number of tasks in Ox and Oz (no decomposition along Oy)
-  INTEGER, PARAMETER :: ims_npro_k = 96
+  INTEGER :: ims_npro_i, ims_npro_j, ims_npro_k ! number of tasks in Ox and Oz (no decomposition along Oy)
   
-  TINTEGER, PARAMETER :: nmax = 10     ! number of repetitions of operations
-
 ! Data below should not be changed
   INTEGER  :: ims_pro, ims_pro_i, ims_pro_j, ims_pro_k ! task positioning
 
@@ -33,13 +29,13 @@ MODULE DNS_MPI
   INTEGER  :: ims_err, ims_tag
 
   INTEGER,  DIMENSION(:  ), ALLOCATABLE :: ims_map_i
-  TINTEGER, DIMENSION(  :), ALLOCATABLE :: ims_size_i
-  TINTEGER, DIMENSION(:,:), ALLOCATABLE :: ims_ds_i, ims_dr_i
+  INTEGER(KIND=4), DIMENSION(  :), ALLOCATABLE :: ims_size_i
+  INTEGER(KIND=4), DIMENSION(:,:), ALLOCATABLE :: ims_ds_i, ims_dr_i
   INTEGER,  DIMENSION(:,:), ALLOCATABLE :: ims_ts_i, ims_tr_i
 
   INTEGER,  DIMENSION(:  ), ALLOCATABLE :: ims_map_k
-  TINTEGER, DIMENSION(  :), ALLOCATABLE :: ims_size_k
-  TINTEGER, DIMENSION(:,:), ALLOCATABLE :: ims_ds_k, ims_dr_k
+  INTEGER(KIND=4), DIMENSION(  :), ALLOCATABLE :: ims_size_k
+  INTEGER(KIND=4), DIMENSION(:,:), ALLOCATABLE :: ims_ds_k, ims_dr_k
   INTEGER,  DIMENSION(:,:), ALLOCATABLE :: ims_ts_k, ims_tr_k
 
 END MODULE DNS_MPI
@@ -55,33 +51,64 @@ PROGRAM VMPI
   
 #include "mpif.h"
 
-  TREAL, DIMENSION(:,:), ALLOCATABLE :: a
-  TREAL, DIMENSION(:),   ALLOCATABLE :: wrk3d
+  REAL(KIND=8), DIMENSION(:,:), ALLOCATABLE :: a
+  REAL(KIND=8), DIMENSION(:),   ALLOCATABLE :: wrk3d
   
 ! -------------------------------------------------------------------
-  TREAL residual                                      ! Control
-  TINTEGER t_srt,t_end,t_dif, PROC_CYCLES, MAX_CYCLES ! Time
-  TINTEGER n
+  REAL(KIND=8) residual                                      ! Control
+  INTEGER(KIND=4) t_srt,t_end,t_dif, PROC_CYCLES, MAX_CYCLES ! Time
+  INTEGER(KIND=4) n
   CHARACTER*64 str
   CHARACTER*256 line
 
   INTEGER ims_npro
-  TREAL dummy
-  TINTEGER idummy, id
+  REAL(KIND=8) dummy, t_x,t_z
+  INTEGER(KIND=4) idummy, id, narg, nmax 
+  CHARACTER*32 cdum
 
 ! ###################################################################
   call MPI_INIT(ims_err)
   call MPI_COMM_SIZE(MPI_COMM_WORLD,ims_npro,ims_err)
   call MPI_COMM_RANK(MPI_COMM_WORLD,ims_pro, ims_err)
-  
-  IF ( ims_npro_i*ims_npro_k .NE. ims_npro ) THEN ! check
-     IF ( ims_pro .EQ. 0 ) THEN
-        WRITE(*,'(a)') 'Inconsistency in total number of PEs'
-     ENDIF     
+
+
+  narg=COMMAND_ARGUMENT_COUNT()   
+  IF ( narg .LT. 4 ) THEN 
+     IF ( ims_pro .EQ. 0 ) WRITE(*,*) 'Usage ./vmpi_rtranspose.x <nrun> <nx> <ny> <nz>'    
+     CALL MPI_FINALIZE(ims_err)  
+     STOP 
+  ENDIF
+
+! Master rank processes input   
+  CALL GETARG(1,cdum); READ(cdum,*) nmax 
+  CALL GETARG(2,cdum); READ(cdum,*) imax_total
+  CALL GETARG(3,cdum); READ(cdum,*) jmax_total
+  CALL GETARG(4,cdum); READ(cdum,*) kmax_total  
+
+  ims_npro_i = 2**((exponent(REAL(ims_npro)))/2)
+  ims_npro_j = 1 
+  ims_npro_k = ims_npro/ims_npro_i  
+  imax = imax_total/ims_npro_i  
+  jmax = jmax_total/ims_npro_j
+  kmax = kmax_total/ims_npro_k 
+
+  IF ( ims_pro .EQ. 0 ) THEN 
+     WRITE(*,*) '=== Initialization of Grid and Decomposition ==='
+     WRITE(*,*) 'GRID:        ', imax_total,' x ',jmax_total,' x ',kmax_total 
+     WRITE(*,*) 'DECOMP: ranks', ims_npro_i,' x ',ims_npro_j,' x ',ims_npro_k 
+     WRITE(*,*) '        grid ', imax,      ' x ',jmax,      ' x ',kmax 
+  ENDIF
+
+  IF ( ims_npro_i*ims_npro_k .NE. ims_npro  .OR. & 
+       ims_npro_i*imax .NE. imax_total .OR. & 
+       ims_npro_k*kmax .NE. kmax_total .OR. & 
+       ims_npro_j*jmax .NE. jmax_total ) THEN ! check
+     IF ( ims_pro .EQ. 0 ) WRITE(*,'(a)') ims_pro,': Inconsistency in Decomposition'  
+     CALL MPI_Barrier(MPI_COMM_WORLD,ims_err)  ! Make sure ims_pro gets to write the message above
      CALL MPI_FINALIZE(ims_err)
      STOP
   ENDIF
-  
+
   CALL DNS_MPI_INITIALIZE
 
   
@@ -92,6 +119,29 @@ PROGRAM VMPI
 ! ###################################################################
 ! Create random array
   CALL RANDOM_NUMBER(a(1:imax*jmax*kmax,1))
+
+
+  IF ( ims_pro .EQ. 0 ) & 
+       WRITE(*,*) 'Executing everything once to get caches / stack / network in production state'
+
+   IF ( ims_npro_k .GT. 1 ) THEN  
+      id = DNS_MPI_K_PARTIAL
+      CALL DNS_MPI_TRPF_K(a(1,1), wrk3d, ims_ds_k(1,id), ims_dr_k(1,id), ims_ts_k(1,id), ims_tr_k(1,id))
+      CALL DNS_MPI_TRPB_K(wrk3d, a(1,2), ims_ds_k(1,id), ims_dr_k(1,id), ims_ts_k(1,id), ims_tr_k(1,id)) 
+   ENDIF
+   IF ( ims_npro_i .GT. 1 ) THEN  
+      id = DNS_MPI_I_PARTIAL
+      CALL DNS_MPI_TRPF_I(a(1,1), wrk3d, ims_ds_i(1,id), ims_dr_i(1,id), ims_ts_i(1,id), ims_tr_i(1,id))
+      CALL DNS_MPI_TRPB_I(wrk3d, a(1,2), ims_ds_i(1,id), ims_dr_i(1,id), ims_ts_i(1,id), ims_tr_i(1,id))
+   ENDIF
+
+   IF ( IMS_PRO .EQ. 0 )THEN 
+      WRITE(*,*) '======' 
+      WRITE(*,*) '===== STARTING MEASUREMENT =====' 
+      WRITE(*,*) '====='
+   END IF
+
+
   
   DO n = 1,nmax
      
@@ -103,20 +153,15 @@ PROGRAM VMPI
         
         CALL SYSTEM_CLOCK(t_srt,PROC_CYCLES,MAX_CYCLES)
 
-        IF ( ims_pro .EQ. 0 ) THEN
-           WRITE(*,'(a)') 'Forward transposition along Ox...'
-        ENDIF
         CALL DNS_MPI_TRPF_I(a(1,1), wrk3d, ims_ds_i(1,id), ims_dr_i(1,id), ims_ts_i(1,id), ims_tr_i(1,id))
-        IF ( ims_pro .EQ. 0 ) THEN
-           WRITE(*,'(a)') 'Backward transposition along Ox...'
-        ENDIF
         CALL DNS_MPI_TRPB_I(wrk3d, a(1,2), ims_ds_i(1,id), ims_dr_i(1,id), ims_ts_i(1,id), ims_tr_i(1,id))
 
         CALL SYSTEM_CLOCK(t_end,PROC_CYCLES,MAX_CYCLES)
         
         idummy = t_end-t_srt
         CALL MPI_REDUCE(idummy, t_dif, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD, ims_err)
-        WRITE(str,'(E13.5E3)') REAL(t_dif)/PROC_CYCLES
+        WRITE(str,'(E13.5E3)') REAL(t_dif)/PROC_CYCLES 
+        t_x = t_x + REAL(t_dif)/PROC_CYCLES 
         
         dummy = MAXVAL(ABS(a(1:imax*jmax*kmax,1)-a(1:imax*jmax*kmax,2)))
         CALL MPI_REDUCE(dummy, residual, 1, MPI_REAL8, MPI_MAX, 0, MPI_COMM_WORLD, ims_err)
@@ -138,13 +183,7 @@ PROGRAM VMPI
         
         CALL SYSTEM_CLOCK(t_srt,PROC_CYCLES,MAX_CYCLES)
         
-        IF ( ims_pro .EQ. 0 ) THEN
-           WRITE(*,'(a)') 'Forward transposition along Oz...'
-        ENDIF
         CALL DNS_MPI_TRPF_K(a(1,1), wrk3d, ims_ds_k(1,id), ims_dr_k(1,id), ims_ts_k(1,id), ims_tr_k(1,id))
-        IF ( ims_pro .EQ. 0 ) THEN
-           WRITE(*,'(a)') 'Backward transposition along Oz...'
-        ENDIF
         CALL DNS_MPI_TRPB_K(wrk3d, a(1,2), ims_ds_k(1,id), ims_dr_k(1,id), ims_ts_k(1,id), ims_tr_k(1,id))
 
         CALL SYSTEM_CLOCK(t_end,PROC_CYCLES,MAX_CYCLES)
@@ -152,6 +191,7 @@ PROGRAM VMPI
         idummy = t_end-t_srt
         CALL MPI_REDUCE(idummy, t_dif, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD, ims_err) 
         WRITE(str,'(E13.5E3)') REAL(t_dif)/PROC_CYCLES 
+        t_z = t_z+REAL(t_dif)/PROC_CYCLES
         
         dummy = MAXVAL(ABS(a(1:imax*jmax*kmax,1)-a(1:imax*jmax*kmax,2)))
         CALL MPI_REDUCE(dummy, residual, 1, MPI_REAL8, MPI_MAX, 0, MPI_COMM_WORLD, ims_err)
@@ -166,8 +206,15 @@ PROGRAM VMPI
      ENDIF
 
   ENDDO
-  
-  CALL MPI_FINALIZE(ims_err)
+
+
+  IF ( ims_pro .EQ. 0 ) THEN 
+     WRITE(*,*) 'X TIMING', t_x/nmax, 'sec.' 
+     WRITE(*,*) 'Z TIMING', t_z/nmax, 'sec.'
+  ENDIF
+
+  CALL MPI_FINALIZE(ims_err) 
+  STOP 
 
 END PROGRAM VMPI
 
@@ -183,8 +230,8 @@ SUBROUTINE DNS_MPI_INITIALIZE
 #include "mpif.h"
 
 ! -----------------------------------------------------------------------
-  TINTEGER id, ip, npage
-  TINTEGER i1, dims(2)
+  INTEGER(KIND=4) id, ip, npage
+  INTEGER(KIND=4) i1, dims(2)
   LOGICAL period(2), remain_dims(2), reorder
 
 ! #######################################################################
@@ -270,13 +317,13 @@ SUBROUTINE DNS_MPI_TYPE_I(ims_npro, imax, npage, nd, md, n1, n2, &
 #include "mpif.h"
 
   INTEGER ims_npro
-  TINTEGER npage, imax, nsize
-  TINTEGER nd, md, n1, n2
-  TINTEGER sdisp(*), rdisp(*)
+  INTEGER(KIND=4) npage, imax, nsize
+  INTEGER(KIND=4) nd, md, n1, n2
+  INTEGER(KIND=4) sdisp(*), rdisp(*)
   INTEGER stype(*), rtype(*)
 
 ! -----------------------------------------------------------------------
-  TINTEGER i
+  INTEGER(KIND=4) i
   INTEGER ims_ss, ims_rs, ims_err
   INTEGER ims_tmp1, ims_tmp2, ims_tmp3
 !  CHARACTER*64 str, line
@@ -335,13 +382,13 @@ SUBROUTINE DNS_MPI_TYPE_K(ims_npro, nmax, npage, nd, md, n1, n2, &
 #include "mpif.h"
 
   INTEGER ims_npro
-  TINTEGER npage, nmax, nsize
-  TINTEGER nd, md, n1, n2
-  TINTEGER sdisp(*), rdisp(*)
+  INTEGER(KIND=4) npage, nmax, nsize
+  INTEGER(KIND=4) nd, md, n1, n2
+  INTEGER(KIND=4) sdisp(*), rdisp(*)
   INTEGER stype(*), rtype(*)
 
 ! -----------------------------------------------------------------------
-  TINTEGER i
+  INTEGER(KIND=4) i
   INTEGER ims_ss, ims_rs, ims_err
   INTEGER ims_tmp1, ims_tmp2, ims_tmp3
 
@@ -399,19 +446,19 @@ SUBROUTINE DNS_MPI_TRPF_K(a, b, dsend, drecv, tsend, trecv)
   
 #include "mpif.h"
 
-  TREAL,    DIMENSION(*),          INTENT(IN)  :: a
-  TREAL,    DIMENSION(*),          INTENT(OUT) :: b
-  TINTEGER, DIMENSION(ims_npro_k), INTENT(IN)  :: dsend, drecv ! displacements
+  REAL(KIND=8),    DIMENSION(*),          INTENT(IN)  :: a
+  REAL(KIND=8),    DIMENSION(*),          INTENT(OUT) :: b
+  INTEGER(KIND=4), DIMENSION(ims_npro_k), INTENT(IN)  :: dsend, drecv ! displacements
   INTEGER,  DIMENSION(ims_npro_k), INTENT(IN)  :: tsend, trecv ! types
   
 ! -----------------------------------------------------------------------
-  TINTEGER n, l
+  INTEGER(KIND=4) n, l
   INTEGER status(MPI_STATUS_SIZE,2*ims_npro_k)
   INTEGER mpireq(                2*ims_npro_k)
   INTEGER ip
 
 #ifdef PROFILE_ON
-  TREAL time_loc_1, time_loc_2
+  REAL(KIND=8) time_loc_1, time_loc_2
 #endif
 
 ! #######################################################################
@@ -461,13 +508,13 @@ SUBROUTINE DNS_MPI_TRPF_I(a, b, dsend, drecv, tsend, trecv)
   
 #include "mpif.h"
   
-  TREAL,    DIMENSION(*),          INTENT(IN)  :: a
-  TREAL,    DIMENSION(*),          INTENT(OUT) :: b
-  TINTEGER, DIMENSION(ims_npro_i), INTENT(IN)  :: dsend, drecv ! displacements
+  REAL(KIND=8),    DIMENSION(*),          INTENT(IN)  :: a
+  REAL(KIND=8),    DIMENSION(*),          INTENT(OUT) :: b
+  INTEGER(KIND=4), DIMENSION(ims_npro_i), INTENT(IN)  :: dsend, drecv ! displacements
   INTEGER,  DIMENSION(ims_npro_i), INTENT(IN)  :: tsend, trecv ! types
   
 ! -----------------------------------------------------------------------
-  TINTEGER n, l
+  INTEGER(KIND=4) n, l
   INTEGER status(MPI_STATUS_SIZE,2*ims_npro_i)
   INTEGER mpireq(                2*ims_npro_i)
   INTEGER ip
@@ -514,19 +561,19 @@ SUBROUTINE DNS_MPI_TRPB_K(b, a, dsend, drecv, tsend, trecv)
   
 #include "mpif.h"
   
-  TREAL,    DIMENSION(*),          INTENT(IN)  :: b
-  TREAL,    DIMENSION(*),          INTENT(OUT) :: a
-  TINTEGER, DIMENSION(ims_npro_k), INTENT(IN)  :: dsend, drecv
+  REAL(KIND=8),    DIMENSION(*),          INTENT(IN)  :: b
+  REAL(KIND=8),    DIMENSION(*),          INTENT(OUT) :: a
+  INTEGER(KIND=4), DIMENSION(ims_npro_k), INTENT(IN)  :: dsend, drecv
   INTEGER,  DIMENSION(ims_npro_k), INTENT(IN)  :: tsend, trecv
   
 ! -----------------------------------------------------------------------
-  TINTEGER n, l
+  INTEGER(KIND=4) n, l
   INTEGER status(MPI_STATUS_SIZE,2*ims_npro_k)
   INTEGER mpireq(                2*ims_npro_k)
   INTEGER ip
 
 #ifdef PROFILE_ON
-  TREAL time_loc_1, time_loc_2
+  REAL(KIND=8) time_loc_1, time_loc_2
 #endif  
 
 ! #######################################################################
@@ -576,13 +623,13 @@ SUBROUTINE DNS_MPI_TRPB_I(b, a, dsend, drecv, tsend, trecv)
   
 #include "mpif.h"
   
-  TREAL,    DIMENSION(*),          INTENT(IN)  :: b
-  TREAL,    DIMENSION(*),          INTENT(OUT) :: a
-  TINTEGER, DIMENSION(ims_npro_i), INTENT(IN)  :: dsend, drecv ! displacements
+  REAL(KIND=8),    DIMENSION(*),          INTENT(IN)  :: b
+  REAL(KIND=8),    DIMENSION(*),          INTENT(OUT) :: a
+  INTEGER(KIND=4), DIMENSION(ims_npro_i), INTENT(IN)  :: dsend, drecv ! displacements
   INTEGER,  DIMENSION(ims_npro_i), INTENT(IN)  :: tsend, trecv ! types
   
 ! -----------------------------------------------------------------------
-  TINTEGER n, l
+  INTEGER(KIND=4) n, l
   INTEGER status(MPI_STATUS_SIZE,2*ims_npro_i)
   INTEGER mpireq(                2*ims_npro_i)
   INTEGER ip
