4211a44bda455f1d0116f8fd02394e604799d8a7
Juan Pedro Mellado <mellado@gmx.de>
2019-04-04 17:38:50 +0200

    Moving to gfortran in the MPIM desktops

--- config/mpipc.cmake
+++ config/mpipc.cmake
@@ -1,59 +1,38 @@
-########################################################
-# Linux Specific Intel Compiler
-# 
-# -C         array index checking at run-time and more
-# -fpe0      stop on floating-point exceptions
-# -traceback give location of error at the end
-# -p         compiles for profiling with gprof
-#            To be set in compilation and linking
-# -g         debug information
-# -fast      Macro to increase performance, but 
-#            numerical results are changed. See SUN block.
-#            To be set in compilation and linking
-# -warn interfaces to check arguments of routines
-# -warn unused     to check declared variables not used
-# -DUSE_FFTW Use FFTW library. 
-#            Add path FFTWROOT as needed
-# -openmp    Use OPENMP (compiler and linker flag)
-#            Add -USE_OPENMP and remove -save
-# -DUSE_MPI Use USE_MPI sections. Add also -DUSE_MPI_IO.
-#            At MPI-M, use wrapper mpif90 instead of ifort
-#            The mpi library at MPI-M works only in little-endian!
-########################################################
 if ( NOT BUILD_TYPE )
-   message( WARNING "Setting BUILD_TYPE to default value." )
+   message( WARNING "Setting CMAKE_BUILD_TYPE to default value." )
    set(BUILD_TYPE BIG)
 endif()
  
-if ( ${BUILD_TYPE} STREQUAL "PARALLEL" ) # compiler for parallel build; assuming intel13
+if ( ${BUILD_TYPE} STREQUAL "PARALLEL" ) # compiler for parallel build
    set(ENV{FC} mpif90)
    set(CMAKE_Fortran_COMPILER mpif90) 
-   set(USER_Fortran_FLAGS          " -fpp -save -nbs -convert little_endian -xHost -warn unused") 
-   set(USER_Fortran_FLAGS_RELEASE  " -O3 ")
+   set(USER_Fortran_FLAGS "-cpp -ffree-form -ffree-line-length-none -fno-automatic")
+   set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -O3 -ffast-math -mtune=native -march=native")
 
    add_definitions(-DUSE_FFTW -DUSE_MPI -DUSE_MPI_IO)
 
    set(CMAKE_BUILD_TYPE RELEASE)
 
 else() # compiler for serial build
-   set(ENV{FC} ifort)
-   set(CMAKE_Fortran_COMPILER ifort) 
-   set(USER_Fortran_FLAGS             " -fpp -save -nbs -xHost -warn unused")  
+   set(ENV{FC} gfortran)
+   set(CMAKE_Fortran_COMPILER gfortran) 
+   set(USER_Fortran_FLAGS "-cpp -ffree-form -ffree-line-length-none -fno-automatic")
 
    add_definitions(-DUSE_FFTW)
 
    if    ( ${BUILD_TYPE} STREQUAL "BIG" )
-     set(USER_Fortran_FLAGS_RELEASE  "-O3 -convert big_endian")
+     set(USER_Fortran_FLAGS_RELEASE "-fconvert=big-endian -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
      
      set(CMAKE_BUILD_TYPE RELEASE) 
      
    elseif( ${BUILD_TYPE} STREQUAL "LITTLE" ) 
-     set(USER_Fortran_FLAGS_RELEASE  "-O3 -convert little_endian") 
+     set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
      
      set(CMAKE_BUILD_TYPE RELEASE) 
      
-   else()	
-     set(USER_Fortran_FLAGS_DEBUG    "-O0 -convert big_endian -pg -debug full -traceback -fpe0 -save-temps")# -warn all")
+   else()
+#     set(USER_Fortran_FLAGS_DEBUG "-O0 -p -ggdb -Wall -fbacktrace -ffpe-trap=invalid,zero,overflow,underflow,precision,denormal")
+     set(USER_Fortran_FLAGS_DEBUG "-O0 -ggdb -Wall -fbacktrace -fconvert=big-endian -ffpe-trap=invalid,zero,overflow")
      
      add_definitions(-D_DEBUG)
      
@@ -69,4 +48,3 @@ set(FFTW_INCLUDE_DIR   "/sw/jessie-x64/fftw-3.3.4-openmp-gccsys/include")
 set(FFTW_LIB           "/sw/jessie-x64/fftw-3.3.4-openmp-gccsys/lib/libfftw3.a")
 set(LIBS ${FFTW_LIB} )
 
-add_definitions(-DRESTRICTKEYWORD=__restrict__)
