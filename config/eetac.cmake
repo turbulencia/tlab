8286b4c8895d87de74e61120b98bad35cd8ba09d
Juan Pedro Mellado <jpmellado@host153.upc.es>
2019-05-24 12:11:58 +0200

    Calculating total fluxes instead of turbulent fluxes for conditional averages.

---
+++ config/eetac.cmake
@@ -0,0 +1,47 @@
+if ( NOT BUILD_TYPE )
+   message( WARNING "Setting CMAKE_BUILD_TYPE to default value." )
+   set(BUILD_TYPE BIG)
+endif()
+ 
+if ( ${BUILD_TYPE} STREQUAL "PARALLEL" ) # compiler for parallel build
+   set(ENV{FC} mpif90)
+   set(CMAKE_Fortran_COMPILER mpif90) 
+   set(USER_Fortran_FLAGS "-cpp -ffree-form -ffree-line-length-none -fno-automatic")
+   set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -O3 -ffast-math -mtune=native -march=native")
+
+   add_definitions(-DUSE_FFTW -DUSE_MPI -DUSE_MPI_IO)
+
+   set(CMAKE_BUILD_TYPE RELEASE)
+
+else() # compiler for serial build
+   set(ENV{FC} gfortran)
+   set(CMAKE_Fortran_COMPILER gfortran) 
+   set(USER_Fortran_FLAGS "-cpp -ffree-form -ffree-line-length-none -fno-automatic")
+
+   add_definitions(-DUSE_FFTW)
+
+   if    ( ${BUILD_TYPE} STREQUAL "BIG" )
+     set(USER_Fortran_FLAGS_RELEASE "-fconvert=big-endian -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
+     
+     set(CMAKE_BUILD_TYPE RELEASE) 
+     
+   elseif( ${BUILD_TYPE} STREQUAL "LITTLE" ) 
+     set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
+     
+     set(CMAKE_BUILD_TYPE RELEASE) 
+     
+   else()
+#     set(USER_Fortran_FLAGS_DEBUG "-O0 -p -ggdb -Wall -fbacktrace -ffpe-trap=invalid,zero,overflow,underflow,precision,denormal")
+     set(USER_Fortran_FLAGS_DEBUG "-O0 -ggdb -Wall -fbacktrace -fconvert=big-endian -ffpe-trap=invalid,zero,overflow")
+     
+     add_definitions(-D_DEBUG)
+     
+     set(CMAKE_BUILD_TYPE DEBUG) 
+     
+   endif() 
+
+endif()     
+
+set(FFTW_INCLUDE_DIR   "/usr/include")
+set(FFTW_LIB           "/usr/lib/x86_64-linux-gnu/libfftw3.a")
+set(LIBS ${FFTW_LIB})
