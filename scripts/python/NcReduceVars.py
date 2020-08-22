6e974be5ea83f9fa2055da4be5ebd08915f77fd2
Juan Pedro Mellado <mellado@gmx.de>
2020-08-22 13:35:39 +0200

    Adding t and y striping to NcReduce.py

--- scripts/python/NcReduceVars.py
+++ scripts/python/NcReduceVars.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 17 12:43:41 2020
-
-@author: jpmellado
-"""
-
 import netCDF4 as nc
 import sys
+
+t_stride = 2    # Reducing also the number of times
+y_stride = 2    # Reducing also the number of levels
 
 # getting data from stdin
 if ( len(sys.argv) <= 1 ):
@@ -19,7 +14,7 @@ setoffiles = sorted(sys.argv[2:])
 
 # processing data
 if   ( filetype == 'flow' ):
-    setofvars = 'rU rV rW rR Rxx Ryy Rzz Rxy Rxz Ryz Wx Wy Wz Wx2 Wy2 Wz2 Tke Buo Prd Eps Trp'.split(' ')
+    setofvars = 'rU rV rW rR Rxx Ryy Rzz Rxy Rxz Ryz Wx Wy Wz Wx2 Wy2 Wz2 Prd Eps Trp'.split(' ')
 elif ( filetype == 'scal' ):
     setofvars = 'rS rS2 Rsu Rsv Rsw rS_y'.split(' ')
 else:
@@ -29,11 +24,11 @@ else:
 for file in setoffiles:
     print("Processing file %s ..." % file)
     file_org = nc.Dataset(file,      'r')
-    file_dst = nc.Dataset(file.replace('.','_r.'), 'w')
+    file_dst = nc.Dataset(file.replace('.nc','_r.nc'), 'w')
 
     # read independent variables from origin nc-file
-    t_org = file_org.variables['t'][:]
-    y_org = file_org.variables['y'][:]
+    t_org = file_org.variables['t'][::t_stride]
+    y_org = file_org.variables['y'][::y_stride]
 
     # create dimensions for destiny nc-file
     file_dst.createDimension('y',len(y_org))
@@ -46,13 +41,13 @@ for file in setoffiles:
     y_dst[:] = y_org[:]
 
     # read and write iteration numbers
-    it_org = file_org.variables['it'][:]
+    it_org = file_org.variables['it'][::t_stride]
     it_dst = file_dst.createVariable('it', 'i4', ('t',))
     it_dst[:] = it_org[:]
 
     # read and write other dependent variables
     for var in setofvars:
-        var_org = file_org.variables[var][:,:]
+        var_org = file_org.variables[var][::t_stride,::y_stride]
         var_dst = file_dst.createVariable(var, 'f8', ('t','y',))
         var_dst[:,:] = var_org[:,:]
 
