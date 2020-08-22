5487d9095425b23b32ae30063e367bd7c43e3832
Juan Pedro Mellado <mellado@gmx.de>
2020-08-22 15:46:40 +0200

    Adding new compact scheme for 2nd order derivative.

--- config/archlinux.cmake
+++ config/archlinux.cmake
@@ -2,12 +2,12 @@ if ( NOT BUILD_TYPE )
    message( WARNING "Setting CMAKE_BUILD_TYPE to default value." )
    set(BUILD_TYPE BIG)
 endif()
- 
+
 if ( ${BUILD_TYPE} STREQUAL "PARALLEL" ) # compiler for parallel build
    set(ENV{FC} mpif90)
-   set(CMAKE_Fortran_COMPILER mpif90) 
+   set(CMAKE_Fortran_COMPILER mpif90)
    set(USER_Fortran_FLAGS "-cpp -ffree-form -ffree-line-length-none -fno-automatic")
-   set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -O3 -ffast-math -mtune=native -march=native")
+   set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -fallow-argument-mismatch -O3 -ffast-math -mtune=native -march=native")
 
    add_definitions(-DUSE_FFTW -DUSE_MPI -DUSE_MPI_IO)
 
@@ -15,32 +15,32 @@ if ( ${BUILD_TYPE} STREQUAL "PARALLEL" ) # compiler for parallel build
 
 else() # compiler for serial build
    set(ENV{FC} gfortran)
-   set(CMAKE_Fortran_COMPILER gfortran) 
+   set(CMAKE_Fortran_COMPILER gfortran)
    set(USER_Fortran_FLAGS "-cpp -ffree-form -ffree-line-length-none -fno-automatic")
 
    add_definitions(-DUSE_FFTW)
 
    if    ( ${BUILD_TYPE} STREQUAL "BIG" )
-     set(USER_Fortran_FLAGS_RELEASE "-fconvert=big-endian -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
-     
-     set(CMAKE_BUILD_TYPE RELEASE) 
-     
-   elseif( ${BUILD_TYPE} STREQUAL "LITTLE" ) 
-     set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
-     
-     set(CMAKE_BUILD_TYPE RELEASE) 
-     
+     set(USER_Fortran_FLAGS_RELEASE "-fconvert=big-endian -fallow-argument-mismatch -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
+
+     set(CMAKE_BUILD_TYPE RELEASE)
+
+   elseif( ${BUILD_TYPE} STREQUAL "LITTLE" )
+     set(USER_Fortran_FLAGS_RELEASE "-fconvert=little-endian -fallow-argument-mismatch -ffpe-summary=none -O3 -ffast-math -mtune=native -march=native")
+
+     set(CMAKE_BUILD_TYPE RELEASE)
+
    else()
 #     set(USER_Fortran_FLAGS_DEBUG "-O0 -p -ggdb -Wall -fbacktrace -ffpe-trap=invalid,zero,overflow,underflow,precision,denormal")
-     set(USER_Fortran_FLAGS_DEBUG "-O0 -ggdb -Wall -fbacktrace -fconvert=big-endian -ffpe-trap=invalid,zero,overflow")
-     
-     add_definitions(-D_DEBUG)
-     
-     set(CMAKE_BUILD_TYPE DEBUG) 
-     
-   endif() 
+     set(USER_Fortran_FLAGS_DEBUG "-O0 -ggdb -Wall -fbacktrace -fconvert=big-endian -fallow-argument-mismatch -ffpe-trap=invalid,zero,overflow")
 
-endif()     
+     add_definitions(-D_DEBUG)
+
+     set(CMAKE_BUILD_TYPE DEBUG)
+
+   endif()
+
+endif()
 
 set(FFTW_INCLUDE_DIR   "/usr/local/include")
 set(FFTW_LIB           "/usr/local/lib/libfftw3.a")
---
+++ fdm/fdm_c2n6ph.f90
@@ -0,0 +1,154 @@
+#include "types.h"
+
+!########################################################################
+!# Tool/Library PADE
+!#
+!########################################################################
+!# HISTORY
+!#
+!# 2020/08/22 - J.P. Mellado
+!#              Created
+!#
+!########################################################################
+!# DESCRIPTION
+!#
+!# Implementation of the second derivative finite difference with
+!# 6th order tridiagonal compact scheme by JCP, Lamballais et al. 2011, JCP 230 3270-3275.
+!#
+!# Eqs. 1,3 with kc = pi**2.
+!# It adds one term in the RHS to better match the exact transfer function,
+!# the resulting FDM being slightly hyperdiffusive instead of hypodiffusive.
+!#
+!# The linear system is normalized to reduce number of operations in RHS
+!#
+!########################################################################
+!# ARGUMENTS
+!#
+!# u    In    function to be diferentiated
+!# d    Out   right-hand side vector of the linear system
+!#
+!########################################################################
+
+! #######################################################################
+! Left-hand side; tridiagonal matrix of the linear system
+! #######################################################################
+#define C_LHS0_L .147077436439844d+1
+#define C_LHS1_L .536071798487849d+0
+
+SUBROUTINE FDM_C2N6PH_LHS(imax, dx, a,b,c)
+
+  IMPLICIT NONE
+
+  TINTEGER,                INTENT(IN) :: imax
+  TREAL,   DIMENSION(imax),INTENT(IN) :: dx
+  TREAL,   DIMENSION(imax),INTENT(OUT):: a,b,c
+
+  ! -------------------------------------------------------------------
+  TINTEGER i
+  TREAL dx1, dxn, dxi
+
+  ! #######################################################################
+  dx1 = dx(1)   *dx(1)
+  dxn = dx(imax)*dx(imax)
+
+  a(1) = dxn*C_LHS1_L
+  a(2) = dx1*C_LHS1_L
+  b(1) = dx1*C_LHS0_L
+
+  DO i = 2,imax-1
+    dxi = dx(i)*dx(i)
+    a(i+1) = dxi*C_LHS1_L
+    b(i)   = dxi*C_LHS0_L
+    c(i-1) = dxi*C_LHS1_L
+  ENDDO
+
+  b(imax)   = dxn*C_LHS0_L
+  c(imax-1) = dxn*C_LHS1_L
+  c(imax)   = dx1*C_LHS1_L
+
+  RETURN
+END SUBROUTINE FDM_C2N6PH_LHS
+
+! #######################################################################
+! Right-hand side; forcing term
+! #######################################################################
+#define C_RHS0_L .281250399533387d+1
+#define C_RHS1_L .422670003525653d+0
+#define C_RHS2_L .164180058587192d-1
+
+SUBROUTINE FDM_C2N6PH_RHS(imax,jkmax, u,d)
+#ifdef USE_OPENMP
+  USE OMP_LIB
+#endif
+
+  IMPLICIT NONE
+
+  TINTEGER,                    INTENT(IN) :: imax, jkmax
+  TREAL, DIMENSION(jkmax,imax),INTENT(IN) :: u
+  TREAL, DIMENSION(jkmax,imax),INTENT(OUT):: d
+
+  ! -------------------------------------------------------------------
+  TINTEGER i, jk, im3, im2, im1, ip1, ip2, ip3,imm1
+
+#ifdef USE_BLAS
+  INTEGER ilen
+  TREAL alpha
+#else
+  TINTEGER srt,end,siz
+#endif
+
+  ! #######################################################################
+#ifdef USE_BLAS
+  ilen = jkmax
+  imm1 = imax-1
+
+  DO i = 1,imax
+    im3 = i-3; im3=im3+imm1; im3=MOD(im3,imax)+1
+    im2 = i-2; im2=im2+imm1; im2=MOD(im2,imax)+1
+    im1 = i-1; im1=im1+imm1; im1=MOD(im1,imax)+1
+    ip1 = i+1; ip1=ip1+imm1; ip1=MOD(ip1,imax)+1
+    ip2 = i+2; ip2=ip2+imm1; ip2=MOD(ip2,imax)+1
+    ip3 = i+3; ip3=ip3+imm1; ip3=MOD(ip3,imax)+1
+
+    !CALL DVEA(ilen, u(1,ip1), 1, u(1,im1), 1, d(1,i), 1)
+    !DVEA is not part of BLAS , but of ESSL -- not supported on intel systems
+    CALL DCOPY(ilen,u(1,ip1),1,d(1,i),1)
+    alpha=C_1_R
+    CALL DAXPY(ilen,alpha,  u(1,im1), 1, d(1,i), 1)
+    alpha =-C_RHS0_L
+    CALL DAXPY(ilen, alpha, u(1,i  ), 1, d(1,i), 1)
+    alpha = C_RHS1_L
+    CALL DAXPY(ilen, alpha, u(1,ip2), 1, d(1,i), 1)
+    CALL DAXPY(ilen, alpha, u(1,im2), 1, d(1,i), 1)
+    alpha =-C_RHS2_L
+    CALL DAXPY(ilen, alpha, u(1,ip3), 1, d(1,i), 1)
+    CALL DAXPY(ilen, alpha, u(1,im3), 1, d(1,i), 1)
+
+  ENDDO
+
+#else
+  !$omp parallel default ( none ) &
+  !$omp private( jk,i,im3,im2,im1,ip1,ip2,ip3,srt,end,siz,imm1 ) &
+  !$omp shared(d,u,imax,jkmax)
+
+  CALL DNS_OMP_PARTITION(jkmax,srt,end,siz)
+
+  DO i = 1,imax
+    im3 = i-3; im3=im3+imm1; im3=MOD(im3,imax)+1
+    im2 = i-2; im2=im2+imm1; im2=MOD(im2,imax)+1
+    im1 = i-1; im1=im1+imm1; im1=MOD(im1,imax)+1
+    ip1 = i+1; ip1=ip1+imm1; ip1=MOD(ip1,imax)+1
+    ip2 = i+2; ip2=ip2+imm1; ip2=MOD(ip2,imax)+1
+    ip3 = i+3; ip3=ip3+imm1; ip3=MOD(ip3,imax)+1
+
+    DO jk = srt,end
+      d(jk,i) = u(jk,ip1) + u(jk,im1) - C_RHS0_L*u(jk,i)+ C_RHS1_L*(u(jk,ip2)+u(jk,im2)) - C_RHS2_L*(u(jk,ip3)+u(jk,im3))
+    ENDDO
+
+  ENDDO
+  !$omp end parallel
+
+#endif
+
+  RETURN
+END SUBROUTINE FDM_C2N6PH_RHS
