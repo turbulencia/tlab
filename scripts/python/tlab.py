d1a045280b2851cdd387629fc5a184588d4ae4f6
Juan Pedro Mellado <mellado@gmx.de>
2021-09-20 10:01:32 +0200

    Adding calculation of zenc to tlab.py.

--- scripts/python/tlab.py
+++ scripts/python/tlab.py
@@ -6,9 +6,26 @@ Created on Tue Dec 17 12:35:54 2019
 @author: jpmellado
 """
 
+import netCDF4 as nc
+import sys
 import numpy as np
 from scipy.linalg import solve_banded
 import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    B0 = 0.005       # Surface buoyancy flux in simulation units.
+    N0 = 3. **0.5    # Buoyancy frequency in the free atmosphere, in simulation units.
+    L0 = (B0/N0**3.)**0.5
+    #
+    sdata = nc.Dataset(sys.argv[1], 'r')
+    sdata.set_auto_mask(False)
+    t  = sdata.variables['t'][:]
+    y  = sdata.variables['y'][:]
+    b  = sdata.variables['rS'][:,:]
+    b_bg  = N0**2.0 *y
+    z_enc = np.sqrt( np.trapz(b[:,:]-b_bg[None,:], y, axis=1) *2.0 /N0**2.0 )
+    #
+    print(z_enc/L0)
 
 def find_nearest(array,value):
     idx = (np.abs(array-value)).argmin()
