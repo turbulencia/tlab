3b06eeef9aacf63820658606efb9e64b148c19bb
zrick <cedrick@posteo.de>
2021-09-29 11:06:00 +0200

    setting boundary values in flow_main.f90 according to velocity_mean

--- src/tools/initialize/flow/flow_main.f90
+++ src/tools/initialize/flow/flow_main.f90
@@ -165,14 +165,18 @@ PROGRAM INIFLOW
 
 ! ###################################################################
 ! Impose noslip+impermeability BCs at Jmin+Jmax (for Dirichlet BCs)
+! --> eliminate perturbations at the boundaries by forcing the fields
+!     to the profile values from velocity_mean
 ! ###################################################################
+  CALL VELOCITY_MEAN(txc(1,1),txc(1,2),txc(1,3),wrk1d) 
+
   nxy = imax*jmax
 
   IF ( bcs_flow_jmin .EQ. DNS_BCS_DIRICHLET) THEN
     DO iq = 1,3
       ip_b = 1
       DO k = 1,kmax
-        q(ip_b:ip_b+imax-1,iq) = C_0_R
+        q(ip_b:ip_b+imax-1,iq) = txc(ip_b:ip_b+imax-1,iq)
         ip_b = ip_b + nxy
       ENDDO
     ENDDO
@@ -182,7 +186,7 @@ PROGRAM INIFLOW
     DO iq = 1,3
       ip_t = imax*(jmax-1) + 1
       DO k = 1,kmax
-        q(ip_t:ip_t+imax-1,iq) = C_0_R
+        q(ip_t:ip_t+imax-1,iq) = txc(ip_t:ip_t+imax-1,iq) 
         ip_t = ip_t + nxy
       ENDDO
     ENDDO
