23e111434a779006c1e9bd8e1976d38f918dc993
Juan Pedro Mellado <mellado@gmx.de>
2021-10-08 11:15:50 +0200

    Adding Coriolis explicit and fixed_scale option to inigrid.

--- src/tools/initialize/grid/grid_read_local.f90
+++ src/tools/initialize/grid/grid_read_local.f90
@@ -35,6 +35,7 @@ SUBROUTINE GRID_READ_LOCAL (inifile, idir, scale, periodic)
   CALL TLAB_WRITE_ASCII(bakfile, 'segments=<number of segments>')
   CALL TLAB_WRITE_ASCII(bakfile, 'periodic=<yes/no>')
   CALL TLAB_WRITE_ASCII(bakfile, 'mirrored=<yes/no>')
+  CALL TLAB_WRITE_ASCII(bakfile, 'fixed_scale=<value>')
 
   CALL SCANINIINT(bakfile, inifile, title, 'segments', '1', idir_opts(1,idir))
 
@@ -46,6 +47,8 @@ SUBROUTINE GRID_READ_LOCAL (inifile, idir, scale, periodic)
   CALL SCANINICHAR(bakfile, inifile, title, 'mirrored', 'no', sRes)
   IF (TRIM(ADJUSTL(sRes)) .eq. 'yes') THEN; idir_opts(3,idir) = 1
   ELSE;                                     idir_opts(3,idir) = 0; ENDIF
+
+  CALL SCANINIREAL(bakfile, inifile, title, 'fixed_scale', '-1.0', fixed_scale(idir))
 
   IF (idir_opts(2,idir).eq.1 .and. idir_opts(3,idir).eq.1) THEN
      CALL TLAB_WRITE_ASCII(efile, 'GRID_READ_LOCAL. Periodicity with mirroring is not supported.')
