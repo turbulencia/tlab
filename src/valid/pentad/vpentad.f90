eb7cd3f951056d417ea52936324ac7e74afa6cab
jkostelecky <j.kostelecky@posteo.de>
2021-12-09 15:14:53 +0100

    Restore original version of vpentad.f90 program.

--- src/valid/pentad/vpentad.f90
+++ src/valid/pentad/vpentad.f90
@@ -5,14 +5,12 @@ PROGRAM VPENTAD
 #include "types.h"
 #include "integers.h"
 
-  TINTEGER,                     parameter :: nmax = 35, len = 5
-  TREAL,    dimension(nmax,5)             :: a
-  TREAL,    dimension(len,nmax)           :: x, f
-  TREAL,    dimension(nmax)               :: g, h
+  TINTEGER,               parameter :: nmax = 32, len = 5
+  TREAL, dimension(nmax,5)          :: a, b, c
+  TREAL, dimension(len,nmax)        :: x, f
 
-  TINTEGER                                :: n, ij, seed
-  TREAL                                   :: RAN0, error, sol, alpha, beta
-  TINTEGER                                :: im2, im1, ip1, ip2, imm1
+  TINTEGER n, ij, seed
+  TREAL RAN0, error, sol
 
 ! ###################################################################
 #define a_a(n) a(n,1)
@@ -21,28 +19,44 @@ PROGRAM VPENTAD
 #define a_d(n) a(n,4)
 #define a_e(n) a(n,5)
 
+#define b_a(n) b(n,1)
+#define b_b(n) b(n,2)
+#define b_c(n) b(n,3)
+#define b_d(n) b(n,4)
+#define b_e(n) b(n,5)
+
   seed = 256
 
-! create diagonals for pentadiagonal Toeplitz matrix
-  ! a_a(1) = RAN0(seed)
-  ! a_b(1) = RAN0(seed)
-  ! a_c(1) = RAN0(seed)
-  ! a_d(1) = RAN0(seed)
-  ! a_e(1) = RAN0(seed)
-! example with modified pentadiagonal 6th-order compact scheme
-  alpha  = 0.6047306974511406
-  beta   = 0.1085589456471229
-  a_a(1) = beta
-  a_b(1) = alpha
-  a_c(1) = 1.0
-  a_d(1) = alpha
-  a_e(1) = beta
-  DO n = 2,nmax
-     a_a(n) = a_a(1)
-     a_b(n) = a_b(1)
-     a_c(n) = a_c(1)
-     a_d(n) = a_d(1)
-     a_e(n) = a_e(1)
+! create diagonals
+  DO n = 1,nmax
+!     a(n) = C_3_R/C_11_R/C_4_R
+!     b(n) = C_12_R/C_11_R
+!     c(n) =-C_51_R/C_22_R
+!     d(n) = C_12_R/C_11_R
+!     e(n) = C_3_R/C_11_R/C_4_R
+     a_a(n) = RAN0(seed)
+     a_b(n) = RAN0(seed)
+     a_c(n) = RAN0(seed)
+     a_d(n) = RAN0(seed)
+     a_e(n) = RAN0(seed)
+  ENDDO
+! check singular case decomposition
+!  a(1) = C_0_R; b(1) = C_0_R; c(1) = C_0_R; d(1) = C_0_R; e(1) = C_0_R
+!  a(nmax) = C_0_R; b(nmax) = C_0_R; c(nmax) = C_0_R; d(nmax) = C_0_R; e(nmax) = C_0_R
+!  c(1) = C_1_R
+
+! padding
+  a_a(1) = C_0_R; a_a(2) = C_0_R; 
+  a_b(1) = C_0_R
+  a_e(nmax-1) = C_0_R; a_e(nmax) = C_0_R; 
+  a_d(nmax) = C_0_R
+
+  DO n = 1,nmax
+     b_a(n) = a_a(n)
+     b_b(n) = a_b(n)
+     b_c(n) = a_c(n)
+     b_d(n) = a_d(n)
+     b_e(n) = a_e(n)
   ENDDO
 
 ! create solution
@@ -53,22 +67,18 @@ PROGRAM VPENTAD
   ENDDO
 
 ! compute forcing term
-  imm1 = nmax - 1 
   DO n = 1,nmax
-     im2 = n-2; im2=im2+imm1; im2=MOD(im2,nmax)+1
-     im1 = n-1; im1=im1+imm1; im1=MOD(im1,nmax)+1
-     ip1 = n+1; ip1=ip1+imm1; ip1=MOD(ip1,nmax)+1
-     ip2 = n+2; ip2=ip2+imm1; ip2=MOD(ip2,nmax)+1
      DO ij = 1,len
-        f(ij,n) = x(ij,im2)*a_a(n) + x(ij,im1)*a_b(n) + x(ij,n)*a_c(n) + x(ij,ip1)*a_d(n) + x(ij,ip2)*a_e(n)
+        f(ij,n) = x(ij,n-2)*a_a(n) + x(ij,n-1)*a_b(n) + x(ij,n)*a_c(n) + x(ij,n+1)*a_d(n) + x(ij,n+2)*a_e(n)
      ENDDO
   ENDDO
 
 ! ###################################################################
-
 ! solve system
-  CALL PENTADPFS(nmax,      a_a(1), a_b(1), a_c(1), a_d(1), a_e(1), g(1), h(1)   )
-  CALL PENTADPSS(nmax, len, a_a(1), a_b(1), a_c(1), a_d(1), a_e(1), g(1), h(1), f)
+!  CALL PENTADFS(nmax,      a_a(1), a_b(1), a_c(1), a_d(1), a_e(1))
+!  CALL PENTADSS(nmax, len, a_a(1), a_b(1), a_c(1), a_d(1), a_e(1), f)
+  CALL PENTADFS2(nmax,      a_a(1), a_b(1), a_c(1), a_d(1), a_e(1))
+  CALL PENTADSS2(nmax, len, a_a(1), a_b(1), a_c(1), a_d(1), a_e(1), f)
 
 ! error
   error = C_0_R
