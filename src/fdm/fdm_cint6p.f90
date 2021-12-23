1bbe75e860510a0f47ad0cdbdf1097667c5931cd
jkostelecky <j.kostelecky@posteo.de>
2021-12-23 09:38:03 +0100

    compact midpoint  interpolation scheme of 6th-order to the left and right with validation routine

---
+++ src/fdm/fdm_cint6p.f90
@@ -0,0 +1,118 @@
+!########################################################################
+!# Tool/Library PADE
+!#
+!########################################################################
+!# HISTORY
+!#
+!# 2021/12/22 - J. Kostelecky
+!#              Created
+!#
+!########################################################################
+!# DESCRIPTION
+!#
+!# Implementation of the finite difference interpolation with
+!# 6th order tridiagonal compact scheme by JCP Lele 1992, periodic.
+!# Interior points according to Eq.C.1.4 (\alpha=3/10, \beta=0, c=0).
+!# System multiplied by 4/3 to eliminate one multiplication in the RHS.
+!#
+!########################################################################
+!# ARGUMENTS 
+!#
+!# u    In    function to be diferentiated
+!# d    Out   right-hand side vector of the linear system
+!#
+!########################################################################
+#include "types.h"
+
+! #######################################################################
+! Left-hand side; tridiagonal matrix of the linear system
+! #######################################################################
+SUBROUTINE FDM_CINT6P_LHS(imax, a,b,c)
+  
+  IMPLICIT NONE
+
+  TINTEGER,                  INTENT(IN ):: imax
+  TREAL,    DIMENSION(imax), INTENT(OUT):: a,b,c
+
+! -------------------------------------------------------------------
+  TINTEGER i
+
+! ###################################################################
+  DO i = 1,imax
+     a(i) = C_2_R / C_5_R ! 3/10
+     b(i) = C_4_R / C_3_R ! 1
+     c(i) = C_2_R / C_5_R ! 3/10
+  ENDDO
+
+  RETURN
+END SUBROUTINE FDM_CINT6P_LHS
+
+! #######################################################################
+! Right-hand side; forcing term ==> interpolation to the right
+! #######################################################################
+SUBROUTINE FDM_CINT6PR_RHS(imax,jkmax, u,d)
+  
+  IMPLICIT NONE
+
+  TINTEGER,                        INTENT(IN ):: imax, jkmax
+  TREAL,    DIMENSION(jkmax,imax), INTENT(IN ):: u
+  TREAL,    DIMENSION(jkmax,imax), INTENT(OUT):: d
+
+! -------------------------------------------------------------------
+  TINTEGER                                    :: i, jk
+  TINTEGER                                    :: im1, ip1, ip2, imm1
+  TREAL                                       :: c0115
+
+! #######################################################################
+
+  c0115 = C_1_R / C_15_R
+
+  imm1 = imax - 1 
+  DO i = 1,imax
+     im1 = i-1; im1=im1+imm1; im1=MOD(im1,imax)+1
+     ip1 = i+1; ip1=ip1+imm1; ip1=MOD(ip1,imax)+1
+     ip2 = i+2; ip2=ip2+imm1; ip2=MOD(ip2,imax)+1
+
+     DO jk = 1,jkmax
+        d(jk,i) = u(jk,ip1) + u(jk,i) + c0115*(u(jk,ip2) + u(jk,im1))
+     ENDDO
+
+  ENDDO
+
+  RETURN
+END SUBROUTINE FDM_CINT6PR_RHS
+
+! #######################################################################
+! Right-hand side; forcing term ==> interpolation to the left
+! #######################################################################
+SUBROUTINE FDM_CINT6PL_RHS(imax,jkmax, u,d)
+  
+  IMPLICIT NONE
+
+  TINTEGER,                        INTENT(IN ):: imax, jkmax
+  TREAL,    DIMENSION(jkmax,imax), INTENT(IN ):: u
+  TREAL,    DIMENSION(jkmax,imax), INTENT(OUT):: d
+
+! -------------------------------------------------------------------
+  TINTEGER                                    :: i, jk
+  TINTEGER                                    :: im1, ip1, im2, imm1
+  TREAL                                       :: c0115
+
+! #######################################################################
+
+  c0115 = C_1_R / C_15_R
+
+  imm1 = imax - 1 
+  DO i = 1,imax
+     im1 = i-1; im1=im1+imm1; im1=MOD(im1,imax)+1
+     im2 = i-2; im2=im2+imm1; im2=MOD(im2,imax)+1
+     ip1 = i+1; ip1=ip1+imm1; ip1=MOD(ip1,imax)+1
+
+     DO jk = 1,jkmax
+        d(jk,i) = u(jk,i) + u(jk,im1) + c0115*(u(jk,ip1) + u(jk,im2))
+     ENDDO
+
+  ENDDO
+
+  RETURN
+END SUBROUTINE FDM_CINT6PL_RHS
---
+++ src/valid/interpol/vinterpol.f90
@@ -0,0 +1,149 @@
+!########################################################################
+!# Valid
+!#
+!########################################################################
+!# HISTORY
+!#
+!# 2021/12/23 - J. Kostelecky
+!#              Created
+!#              
+!#
+!########################################################################
+!# DESCRIPTION
+!#
+!# Validate compact interpolation schemes.
+!#
+!########################################################################
+!# ARGUMENTS 
+!#
+!########################################################################
+#include "types.h"
+#include "dns_const.h"
+
+PROGRAM INTERPOL
+
+  USE TLAB_TYPES, ONLY : grid_dt
+  
+  IMPLICIT NONE
+ 
+#include "integers.h"
+  
+  TYPE(grid_dt)                      :: g
+   
+  TINTEGER                           :: jmax,kmax, i, l, test_type
+  
+  TREAL                              :: lambda, error, sol
+
+  TINTEGER, PARAMETER                :: imax=10, len=1, inb_grid=44
+  
+  TREAL,    DIMENSION(imax,inb_grid) :: x
+  TREAL,    DIMENSION(imax)          :: x_int, x_aux
+  TREAL,    DIMENSION(len,imax)      :: u, u_int, u_aux, u_a, u_b, u_c
+  TREAL,    DIMENSION(imax,5)        :: wrk1d
+  TREAL,    DIMENSION(len)           :: wrk2d
+
+! ###################################################################
+! Initialize
+  g%size     = imax 
+  g%scale    = C_1_R
+  g%uniform  = .TRUE.
+  jmax       = 1
+  kmax       = 1
+  g%mode_fdm = FDM_COM6_JACOBIAN 
+   
+! Valid stettings
+  g%periodic = .TRUE.
+  lambda     = 1               
+  test_type  = 2
+ 
+! ###################################################################
+! Initialize grid    
+  IF ( g%periodic ) THEN
+    DO i = 1,imax
+      x(i,1) = M_REAL(i-1)/M_REAL(imax)*g%scale
+    ENDDO
+  ELSE
+    DO i = 1,imax
+      x(i,1) = M_REAL(i-1)/M_REAL(imax-1)*g%scale
+    ENDDO
+  ENDIF
+
+  CALL FDM_INITIALIZE(x, g, wrk1d)
+
+! Interpolation grid -- midpoints
+  IF ( g%periodic ) THEN
+    DO i = 1,imax
+      x_int(i) = g%nodes(i) + 0.5 * g%jac(i,1)
+    ENDDO
+  ELSE
+    DO i = 1,imax-1
+      x_int(i) = g%nodes(i) + 0.5 * g%jac(i,1)
+    ENDDO
+    x_int(imax) = C_0_R
+  ENDIF
+  x_aux(:) = x_int(:)
+
+! Define the function on both grids
+  DO i = 1,imax
+    DO l = 1,len
+      u(l,i)     = SIN(C_2_R*C_PI_R/g%scale*lambda*g%nodes(i))
+      u_int(l,i) = SIN(C_2_R*C_PI_R/g%scale*lambda*x_int(i)  )
+      u_aux(l,i) = u_int(l,i)
+    ENDDO
+  ENDDO
+
+! Switch grid and u (for test_type==2)
+  IF ( test_type .EQ. 2 ) THEN 
+    DO i = 1,imax
+      x_int(i) = x(i,1) 
+      x(i,1)   = x_aux(i) 
+      DO l = 1,len
+        u_int(l,i) = u(l,i) 
+        u(l,i)     = u_aux(l,i) 
+      ENDDO
+    ENDDO
+  ENDIF
+! ###################################################################
+! Testing interpolation
+  IF (     test_type .EQ. 1 ) THEN 
+    WRITE(*,*) '.......... Interpolation to the right .......... '
+    CALL FDM_CINT6P_LHS( imax,  wrk1d(1,1),wrk1d(1,2),wrk1d(1,3))
+    CALL FDM_CINT6PR_RHS(imax,len, u, u_a)
+    !
+    CALL TRIDPFS(imax,      wrk1d(1,1),wrk1d(1,2),wrk1d(1,3),wrk1d(1,4),wrk1d(1,5))
+    CALL TRIDPSS(imax, len, wrk1d(1,1),wrk1d(1,2),wrk1d(1,3),wrk1d(1,4),wrk1d(1,5), u_a(1,1),wrk2d(1))
+  ELSEIF ( test_type .EQ. 2 ) THEN 
+    WRITE(*,*) '.......... Interpolation to the left ........... '
+    CALL FDM_CINT6P_LHS( imax,  wrk1d(1,1),wrk1d(1,2),wrk1d(1,3))
+    CALL FDM_CINT6PL_RHS(imax,len, u, u_a)
+    !
+    CALL TRIDPFS(imax,      wrk1d(1,1),wrk1d(1,2),wrk1d(1,3),wrk1d(1,4),wrk1d(1,5))
+    CALL TRIDPSS(imax, len, wrk1d(1,1),wrk1d(1,2),wrk1d(1,3),wrk1d(1,4),wrk1d(1,5), u_a(1,1),wrk2d(1))
+  ENDIF
+! ###################################################################
+! IO - Error and function values
+  OPEN(20,file='interpol.dat')
+  error = C_0_R
+  sol   = C_0_R
+  DO i = 1,imax
+    DO l = 1,len
+      WRITE(20,1000) x(i,1), x_int(i), u(l,i), u_int(l,i), u_a(l,i), u_a(l,i) - u_int(l,i)
+      u_c(l,i) = ABS(u_a(l,i) - u_int(l,i))
+      error = error + u_c(l,i)   * u_c(l,i)
+      sol   = sol   + u_int(l,i) * u_int(l,i)
+    ENDDO
+  ENDDO
+  CLOSE(20)
+
+  WRITE(*,2000) 'Solution L2-norm ...........:', SQRT(g%jac(1,1)*sol) / M_REAL(len)
+  IF ( sol .EQ. C_0_R ) STOP
+  WRITE(*,2000) 'Error L2-norm ..............:', SQRT(g%jac(1,1)*error) 
+  WRITE(*,2000) 'Error Linf-norm ............:', MAXVAL(u_c(1,1:imax))
+  WRITE(*,2000) 'Relative error .............:', sqrt(error)/sqrt(sol)
+
+  STOP
+
+1000 FORMAT(6(1x,e16.10))
+2000 FORMAT(1(1x,A,3x,e16.10))
+
+END PROGRAM INTERPOL
