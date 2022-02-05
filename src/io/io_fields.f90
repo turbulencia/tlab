31a5c3aff0455018d0b3d84b1d7358d1000dda72
Juan Pedro Mellado <mellado@gmx.de>
2022-02-05 09:54:38 +0100

    Adding io_int1 to io_fields.

--- src/io/io_fields.f90
+++ src/io/io_fields.f90
@@ -18,10 +18,9 @@
 
 MODULE IO_FIELDS
 
-  USE TLAB_CONSTANTS, ONLY : wfile, efile
+  USE TLAB_CONSTANTS, ONLY : lfile, wfile, efile
   USE TLAB_PROCS, ONLY : TLAB_STOP, TLAB_WRITE_ASCII
 #ifdef USE_MPI
-  USE TLAB_CONSTANTS, ONLY : lfile
   USE TLAB_MPI_VARS, ONLY : ims_err
   USE TLAB_MPI_VARS, ONLY : ims_pro, ims_npro_i, ims_npro_k
   USE TLAB_MPI_VARS, ONLY : ims_offset_i, ims_offset_j, ims_offset_k
@@ -33,13 +32,15 @@ MODULE IO_FIELDS
   IMPLICIT NONE
 
   PUBLIC :: IO_READ_FIELD_SUBARRAY, IO_WRITE_FIELD_SUBARRAY
-  PUBLIC :: IO_READ_HEADER, IO_WRITE_HEADER
   PUBLIC :: IO_READ_FIELD_XPENCIL, IO_WRITE_FIELD_XPENCIL
+  PUBLIC :: IO_READ_FIELD_INT1, IO_WRITE_FIELD_INT1
 
   PRIVATE
 
   TINTEGER header_offset
   TINTEGER nx_total, ny_total, nz_total
+  CHARACTER*32 str
+  CHARACTER*128 line
 
 #ifdef USE_MPI
 #include "mpif.h"
@@ -225,6 +226,89 @@ CONTAINS
 #undef LOC_UNIT_ID
 #undef LOC_STATUS
 
+#define LOC_UNIT_ID 34
+#define LOC_STATUS 'old'
+  !########################################################################
+  !########################################################################
+  SUBROUTINE IO_READ_FIELD_INT1(name, iheader, nx,ny,nz,nt, isize,params, a)
+    IMPLICIT NONE
+
+    CHARACTER(LEN=*) name
+    TINTEGER,   INTENT(IN   ) :: iheader, nx,ny,nz,nt
+    TINTEGER,   INTENT(INOUT) :: isize
+    TREAL,      INTENT(INOUT) :: params(isize)
+    INTEGER(1), INTENT(  OUT) :: a(nx*ny*nz)
+
+    ! ###################################################################
+#ifdef USE_MPI
+    nx_total = nx*ims_npro_i
+    ny_total = ny
+    nz_total = nz*ims_npro_k
+#else
+    nx_total = nx
+    ny_total = ny
+    nz_total = nz
+#endif
+
+    line = 'Reading field '//TRIM(ADJUSTL(name))//' of size'
+    WRITE(str,*) nx_total; line = TRIM(ADJUSTL(line))//' '//TRIM(ADJUSTL(str))
+    WRITE(str,*) ny_total; line = TRIM(ADJUSTL(line))//'x'//TRIM(ADJUSTL(str))
+    WRITE(str,*) nz_total; line = TRIM(ADJUSTL(line))//'x'//TRIM(ADJUSTL(str))//'...'
+    CALL TLAB_WRITE_ASCII(lfile, line)
+
+    ! -------------------------------------------------------------------
+    ! header
+#ifdef USE_MPI
+    IF ( ims_pro .EQ. 0 ) THEN
+#endif
+
+      header_offset = 0
+#include "dns_open_file.h"
+      REWIND(LOC_UNIT_ID)
+      IF ( iheader .GT. 0 ) THEN
+        CALL IO_READ_HEADER(LOC_UNIT_ID, header_offset, nx_total,ny_total,nz_total,nt, params)
+        isize = (header_offset - 5*SIZEOFINT)/SIZEOFREAL ! Size of array params
+      ENDIF
+      CLOSE(LOC_UNIT_ID)
+
+#ifdef USE_MPI
+    ENDIF
+    CALL MPI_BCAST(params, isize, MPI_REAL8, 0, MPI_COMM_WORLD, ims_err)
+
+    ! -------------------------------------------------------------------
+    ! field
+    CALL MPI_BCAST(header_offset, 1, MPI_INTEGER4, 0, MPI_COMM_WORLD, ims_err)
+    mpio_disp = header_offset*SIZEOFBYTE ! Displacement to start of field
+
+    ndims = 3
+    sizes(1)   = nx_total;     sizes(2)   = ny_total;     sizes(3)   = nz_total
+    locsize(1) = nx;           locsize(2) = ny;           locsize(3) = nz
+    offset(1)  = ims_offset_i; offset(2)  = ims_offset_j; offset(3)  = ims_offset_k
+
+    CALL MPI_Type_create_subarray(ndims, sizes, locsize, offset, &
+        MPI_ORDER_FORTRAN, MPI_INTEGER1, subarray, ims_err)
+    CALL MPI_Type_commit(subarray, ims_err)
+
+    mpio_locsize = nx*ny*nz
+    CALL MPI_FILE_OPEN(MPI_COMM_WORLD, name, MPI_MODE_RDONLY, MPI_INFO_NULL, mpio_fh, ims_err)
+    CALL MPI_File_set_view(mpio_fh, mpio_disp, MPI_INTEGER1, subarray, 'native', MPI_INFO_NULL, ims_err)
+    CALL MPI_File_read_all(mpio_fh, a, mpio_locsize, MPI_INTEGER1, status, ims_err)
+    CALL MPI_File_close(mpio_fh, ims_err)
+
+#else
+#include "dns_open_file.h"
+    header_offset = header_offset +1
+    READ(LOC_UNIT_ID,POS=header_offset) a
+    CLOSE(LOC_UNIT_ID)
+
+#endif
+
+    RETURN
+  END SUBROUTINE IO_READ_FIELD_INT1
+
+#undef LOC_UNIT_ID
+#undef LOC_STATUS
+
   !########################################################################
   !########################################################################
 #define LOC_UNIT_ID 55
@@ -384,6 +468,90 @@ CONTAINS
 
     RETURN
   END SUBROUTINE IO_WRITE_FIELD_XPENCIL
+
+#undef LOC_UNIT_ID
+#undef LOC_STATUS
+
+#define LOC_UNIT_ID 35
+#define LOC_STATUS 'unknown'
+  !########################################################################
+  !########################################################################
+  SUBROUTINE IO_WRITE_FIELD_INT1(name, iheader, nx,ny,nz,nt, isize,params, a)
+    IMPLICIT NONE
+
+    CHARACTER(LEN=*) name
+    TINTEGER,   INTENT(IN   ) :: iheader, nx,ny,nz,nt, isize
+    TREAL,      INTENT(IN   ) :: params(isize)
+    INTEGER(1), INTENT(IN   ) :: a(nx*ny*nz)
+
+    ! ###################################################################
+#ifdef USE_MPI
+    nx_total = nx*ims_npro_i
+    ny_total = ny
+    nz_total = nz*ims_npro_k
+#else
+    nx_total = nx
+    ny_total = ny
+    nz_total = nz
+#endif
+
+    line = 'Writing field '//TRIM(ADJUSTL(name))//' of size'
+    WRITE(str,*) nx_total; line = TRIM(ADJUSTL(line))//' '//TRIM(ADJUSTL(str))
+    WRITE(str,*) ny_total; line = TRIM(ADJUSTL(line))//'x'//TRIM(ADJUSTL(str))
+    WRITE(str,*) nz_total; line = TRIM(ADJUSTL(line))//'x'//TRIM(ADJUSTL(str))//'...'
+    CALL TLAB_WRITE_ASCII(lfile, line)
+
+    ! -------------------------------------------------------------------
+    ! header
+#ifdef USE_MPI
+    IF ( ims_pro .EQ. 0 ) THEN
+#endif
+
+      header_offset = 0
+#include "dns_open_file.h"
+      IF ( iheader .GT. 0 ) THEN
+        CALL IO_WRITE_HEADER(LOC_UNIT_ID, isize, nx_total,ny_total,nz_total,nt, params)
+        header_offset = 5*SIZEOFINT + isize*SIZEOFREAL
+      ENDIF
+      CLOSE(LOC_UNIT_ID)
+
+#ifdef USE_MPI
+    ENDIF
+
+    ! -------------------------------------------------------------------
+    ! field
+    CALL MPI_BCAST(header_offset, 1, MPI_INTEGER4, 0, MPI_COMM_WORLD, ims_err)
+    mpio_disp = header_offset*SIZEOFBYTE
+
+    CALL MPI_BARRIER(MPI_COMM_WORLD, ims_err)
+
+    ndims = 3
+    sizes(1)   = nx_total;     sizes(2)   = ny_total;     sizes(3)   = nz_total
+    locsize(1) = nx;           locsize(2) = ny;           locsize(3) = nz
+    offset(1)  = ims_offset_i; offset(2)  = ims_offset_j; offset(3)  = ims_offset_k
+
+    CALL MPI_Type_create_subarray(ndims, sizes, locsize, offset, &
+        MPI_ORDER_FORTRAN, MPI_INTEGER1, subarray, ims_err)
+    CALL MPI_Type_commit(subarray, ims_err)
+
+    mpio_locsize = nx*ny*nz
+    CALL MPI_FILE_OPEN(MPI_COMM_WORLD, name, IOR(MPI_MODE_WRONLY,MPI_MODE_CREATE), MPI_INFO_NULL, mpio_fh, ims_err)
+    CALL MPI_File_set_view(mpio_fh, mpio_disp, MPI_INTEGER1, subarray, 'native', MPI_INFO_NULL, ims_err)
+    CALL MPI_File_write_all(mpio_fh, a, mpio_locsize, MPI_INTEGER1, status, ims_err)
+    CALL MPI_File_close(mpio_fh, ims_err)
+
+#else
+#include "dns_open_file.h"
+    header_offset = header_offset +1
+    WRITE(LOC_UNIT_ID,POS=header_offset) a
+    CLOSE(LOC_UNIT_ID)
+#endif
+
+    RETURN
+  END SUBROUTINE IO_WRITE_FIELD_INT1
+
+#undef LOC_UNIT_ID
+#undef LOC_STATUS
 
   !########################################################################
   !########################################################################
