07c36cfd5af9059b74468a4dd8caff6a1ac3e74a
Juan Pedro Mellado <mellado@gmx.de>
2023-03-11 07:32:52 +0100

    Cleaning iniflow.

--- src/tools/initialize/flow/flow_main.f90
+++ src/tools/initialize/flow/flow_main.f90
@@ -7,6 +7,7 @@ program INIFLOW
     use TLAB_CONSTANTS
     use TLAB_VARS
     use TLAB_ARRAYS
+    use TLAB_POINTERS, only: e, rho, p, T
     use TLAB_PROCS
 #ifdef USE_MPI
     use MPI
@@ -14,20 +15,11 @@ program INIFLOW
 #endif
     use THERMO_VARS, only: imixture
     use IO_FIELDS
-#ifdef USE_CGLOC
-    use CG_GLOBAL, only: cg_unif, cg_ord
-#endif
     use OPR_FOURIER
     use FLOW_LOCAL
     use FLOW_MEAN
     
     implicit none
-
-    ! -------------------------------------------------------------------
-    ! Additional local arrays
-#ifdef USE_CGLOC
-    real(wp), dimension(:), allocatable, save :: ci, cj, ck, ipos, jpos, kpos
-#endif
 
     !########################################################################
     call TLAB_START()
@@ -42,8 +34,10 @@ program INIFLOW
     inb_wrk2d = MAX(inb_wrk2d, 3)
     isize_wrk3d = isize_txc_field
 
-    if (flag_u == 0) then; inb_txc = 2
-    else; inb_txc = 8
+    if (flag_u == 0) then
+        inb_txc = 2
+    else
+        inb_txc = 8
     end if
 
     call TLAB_ALLOCATE(C_FILE_LOC)
@@ -70,30 +64,8 @@ program INIFLOW
             call OPR_FOURIER_INITIALIZE()
 
         else
-#ifdef USE_CGLOC
-            allocate (ci(isize_field*2))
-            allocate (cj(isize_field*2))
-            allocate (ck(isize_field*2))
-            allocate (ipos(imax*4))
-            allocate (jpos(jmax*4))
-            allocate (kpos(kmax*4))
-
-            if (.not. g(1)%uniform .or. .not. g(2)%uniform) then
-                call TLAB_WRITE_ASCII(lfile, 'Initializing conjugate gradient, non-uniform grid, second-order.')
-                cg_unif = 1; cg_ord = 2
-                ! to be rewritten in terms of grid derived type
-                ! CALL CGBC2(cg_unif, imode_fdm, imax,jmax,kmax,g(3)%size, &
-                !      i1bc,j1bc,k1bc, scalex,scaley,scalez, dx,dy,dz, ipos,jpos,kpos,ci,cj,ck, wrk2d)
-            else
-                call TLAB_WRITE_ASCII(lfile, 'Initializing conjugate gradient, uniform grid, fourth-order.')
-                cg_unif = 0; cg_ord = 4
-                ! CALL CGBC4(cg_unif, imax,jmax,kmax,g(3)%size, &
-                !      i1bc,j1bc,k1bc, scalex,scaley,scalez, dx,dy,dz, ipos,jpos,kpos,ci,cj,ck, wrk2d)
-            end if
-#else
             call TLAB_WRITE_ASCII(efile, C_FILE_LOC//'. CG routines needed.')
             call TLAB_STOP(DNS_ERROR_OPTION)
-#endif
         end if
 
     end if
@@ -123,16 +95,11 @@ program INIFLOW
     if (imode_eqns == DNS_EQNS_TOTAL .or. imode_eqns == DNS_EQNS_INTERNAL) then
         call TLAB_WRITE_ASCII(lfile, 'Initializing pressure and density.')
 
-        ! for readibility: energy, density, pressure and temperature
-#define e_loc      q(:, 4)
-#define r_loc      q(:, 5)
-#define p_loc      q(:, 6)
-#define T_loc      q(:, 7)
-        call PRESSURE_MEAN(p_loc, T_loc, s)
-        call DENSITY_MEAN(r_loc, p_loc, T_loc, s, txc)
+        call PRESSURE_MEAN(p, T, s)
+        call DENSITY_MEAN(rho, p, T, s, txc)
 
         if (flag_u /= 0) then
-            call PRESSURE_FLUCTUATION(q(1, 1), q(1, 2), q(1, 3), r_loc, p_loc, txc(1, 1), &
+            call PRESSURE_FLUCTUATION(q(1, 1), q(1, 2), q(1, 3), rho, p, txc(1, 1), &
                                       txc(1, 2), txc(1, 3), txc(1, 4), txc(1, 5))
         end if
 
@@ -141,12 +108,12 @@ program INIFLOW
         end if
 
         if (flag_t /= 0) then
-            call DENSITY_FLUCTUATION(s, p_loc, r_loc, txc(1, 1), txc(1, 2))
+            call DENSITY_FLUCTUATION(s, p, rho, txc(1, 1), txc(1, 2))
         end if
 
         ! Calculate specfic energy. Array s should contain the species fields at this point.
-        call THERMO_THERMAL_TEMPERATURE(imax, jmax, kmax, s, p_loc, r_loc, txc(1, 1))
-        call THERMO_CALORIC_ENERGY(imax, jmax, kmax, s, txc(1, 1), e_loc)
+        call THERMO_THERMAL_TEMPERATURE(imax, jmax, kmax, s, p, rho, txc(1, 1))
+        call THERMO_CALORIC_ENERGY(imax, jmax, kmax, s, txc(1, 1), e)
 
     end if
 
