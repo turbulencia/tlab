c5e1e29451671f8f638ea87b663472a3b8d39b4a
Juan Pedro Mellado <mellado@gmx.de>
2023-03-12 15:34:40 +0100

    Further cleaning to improove readibility of source code.

--- src/modules/tlab_vars.f90
+++ src/modules/tlab_vars.f90
@@ -39,9 +39,12 @@ module TLAB_VARS
 ! Arrays sizes
 ! ###################################################################
 ! fields
-    integer(wi) :: imax, jmax, kmax, isize_field    ! 3D fields sizes locally per processor
-    integer(wi) :: inb_flow, inb_flow_array         ! # of prognostic and diagnostic 3d flow arrays
-    integer(wi) :: inb_scal, inb_scal_array         ! # of prognostic and diagnostic 3d scal arrays
+    integer(wi) :: imax, jmax, kmax                 ! number of grid nodes per direction locally per processor
+    integer(wi) :: isize_field                      ! =imax*jmax*kmax, 3D fields sizes locally per processor
+    integer(wi) :: inb_flow                         ! # of prognostic 3d flow fields (flow evolution equations)
+    integer(wi) :: inb_flow_array                   ! >= inb_flow, # of prognostic and diagnostic 3d flow arrays
+    integer(wi) :: inb_scal                         ! # of prognostic 3d scal fields (scal evolution equations)
+    integer(wi) :: inb_scal_array                   ! >= inb_scal, # of prognostic and diagnostic 3d scal arrays
 
 ! auxiliary arrays
     integer(wi) :: isize_wrk1d, inb_wrk1d           ! 1D scratch arrays
