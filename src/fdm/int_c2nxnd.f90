7d77f9eaad69a8959807319d902cbfab74a4b19a
Juan Pedro Mellado <mellado@gmx.de>
2023-06-07 17:24:56 +0200

    Starting the implementation of compact FDMs in boudary condition of int_compact_direct.

--- src/fdm/int_c2nxnd.f90
+++ src/fdm/int_c2nxnd.f90
@@ -29,7 +29,7 @@ subroutine INT_C2NXND_LHS_E(imax, x, ibc, lhs, rhs, lambda2, a, b, c, d, e, f1, 
     integer(wi), intent(in) :: imax       ! original size; here using only 2:imax-1
     real(wp), intent(in) :: x(imax)
     integer, intent(in) :: ibc
-    real(wp), intent(in) :: lhs(imax, 3)
+    real(wp), intent(inout) :: lhs(imax, 3)
     real(wp), intent(in) :: rhs(imax, 4)
     real(wp) lambda2
     real(wp), intent(out) :: a(imax), b(imax), c(imax), d(imax), e(imax)  ! diagonals
@@ -37,8 +37,7 @@ subroutine INT_C2NXND_LHS_E(imax, x, ibc, lhs, rhs, lambda2, a, b, c, d, e, f1, 
 
 ! -------------------------------------------------------------------
     integer(wi) i
-    real(wp) dummy1, dummy2, pprime, coef(4)
-    integer(wi) stencil(4), size_of_stencil
+    real(wp) dummy1, dummy2, pprime, coef(5)
 
 ! ###################################################################
 ! -------------------------------------------------------------------
@@ -97,56 +96,109 @@ subroutine INT_C2NXND_LHS_E(imax, x, ibc, lhs, rhs, lambda2, a, b, c, d, e, f1, 
     ! -------------------------------------------------------------------
     ! Corrections to the BCS_DD to account for Neumann using third-order fdm for derivative at the boundary
     if (any([BCS_ND, BCS_NN] == ibc)) then
-        ! Coefficients in FDM p'_1= c_1 p_1 + c_2 p_2 + c_3 p_3 + ...
-        ! coef = [-1.5_wp, 2.0_wp, -0.5_wp, 0.0_wp]/dx(1,2)                       ! second-order, uniform
-        ! coef = [-11.0_wp/6.0_wp, 3.0_wp, -1.5_wp, 1.0_wp/3.0_wp]/dx(1, 1)       ! third-order, uniform
-        ! stencil(1:3) = [1, 2, 3]; size_of_stencil = 3                           ! second-order
-        stencil(1:4) = [1, 2, 3, 4]; size_of_stencil = 4                        ! third-order
+        ! Coefficients in FDM p'_1= b_1 p_1 + b_2 p_2 + b_3 p_3 + b_4 p_4 + a_2 p''_2
         coef(:) = 0.0_wp
-        do i = 1, size_of_stencil
-            coef(i) = Lag_p(x, 1, i, stencil(1:size_of_stencil))
-        end do
+        ! coef(1:3) = coef_e1n2_biased(x, 1)              ! second-order
+        coef(1:4) = coef_e1n3_biased(x, 1)              ! third-order
+        ! coef(1:4) = [-35.0_wp/6.0_wp, 11.0_wp, -33.0_wp/6.0_wp, 2.0_wp/6.0_wp]/(x(2) - x(1))
+        ! coef(5) = 4.0_wp*(x(2) - x(1))
+
         ! Data to calculate p_1 in terms of p_2, p_3, p_4 and p'_1
-        c(1) = -coef(2)/coef(1)
+        c(1) = -coef(2)/coef(1) - lambda2*coef(5)/coef(1)       ! d + lambda^2h^2 e in notes, e only 1 component
         d(1) = -coef(3)/coef(1)
         e(1) = -coef(4)/coef(1)
         pprime = 1.0_wp/coef(1)
+        coef(5) = -coef(5)/coef(1)                              ! coefficient for p''_1
+
         ! Derived coefficients; see notes
-        c(2) = c(2) - c(1)*f1(2)                                ! in reduced matrix; the minus sign comes from def of f1
+        c(2) = c(2) - c(1)*f1(2)                                ! in reduced C matrix; the minus sign comes from def of f1
         d(2) = d(2) - d(1)*f1(2)
         e(2) = e(2) - e(1)*f1(2)
         b(3) = b(3) - c(1)*f1(3)
         c(3) = c(3) - d(1)*f1(3)
         d(3) = d(3) - e(1)*f1(3)
+
+        lhs(2, 2) = lhs(2, 2) + coef(5)*f1(2)                   ! in reduced A matrix; the plus sign comes from def of f1
+        lhs(3, 1) = lhs(3, 1) + coef(5)*f1(3)
+
         f1(:) = pprime*f1(:)                                    ! for particular solutions
+
     end if
     if (any([BCS_DN, BCS_NN] == ibc)) then
-        ! Coefficients in FDM p'_n=c_1 p_n + c_2 p_{n-1} + c_3 p_{n-2} +...
-        ! coef = [1.5_wp, -2.0_wp, 0.5_wp, 0.0_wp]/dx(imax, 1)                        ! second-order
-        ! coef = [11.0_wp/6.0_wp, -3.0_wp, 1.5_wp, -1.0_wp/3.0_wp]/dx(imax, 1)        ! third-order
-        ! stencil(1:3) = [imax, imax - 1, imax - 2]; size_of_stencil = 3              ! second-order
-        stencil(1:4) = [imax, imax - 1, imax - 2, imax - 3]; size_of_stencil = 4    ! third-order
+        ! Coefficients in FDM p'_n = b_1 p_n + b_2 p_{n-1} + b_3 p_{n-2} +...
         coef(:) = 0.0_wp
-        do i = 1, size_of_stencil
-            coef(i) = Lag_p(x, imax, stencil(i), stencil(1:size_of_stencil))
-        end do
+        ! coef(1:3) = coef_e1n2_biased(x, imax, backwards=.true.)
+        coef(1:4) = coef_e1n3_biased(x, imax, backwards=.true.)
         ! Data to calculate p_n in terms of p_{n-1}, p_{n-2} and p'_n
-        c(imax) = -coef(2)/coef(1)
+        c(imax) = -coef(2)/coef(1) - lambda2*coef(5)/coef(1)
         b(imax) = -coef(3)/coef(1)
         a(imax) = -coef(4)/coef(1)
         pprime = 1.0_wp/coef(1)
+        coef(5) = -coef(5)/coef(1)                              ! coefficient for p''_n
+
         ! Derived coefficients; see notes
-        a(imax - 1) = a(imax - 1) - a(imax)*f2(imax - 1)        ! in reduced matrix; the minus sign comes from def of f2
+        a(imax - 1) = a(imax - 1) - a(imax)*f2(imax - 1)        ! in reduced C matrix; the minus sign comes from def of f2
         b(imax - 1) = b(imax - 1) - b(imax)*f2(imax - 1)
         c(imax - 1) = c(imax - 1) - c(imax)*f2(imax - 1)
         b(imax - 2) = b(imax - 2) - a(imax)*f2(imax - 2)
         c(imax - 2) = c(imax - 2) - b(imax)*f2(imax - 2)
         d(imax - 2) = d(imax - 2) - c(imax)*f2(imax - 2)
+
+        lhs(imax - 1, 2) = lhs(imax - 1, 2) + coef(5)*f2(imax - 1)  ! in reduced A matrix; the plus sign comes from def of f2
+        lhs(imax - 2, 3) = lhs(imax - 2, 3) + coef(5)*f2(imax - 2)
+
         f2(:) = pprime*f2(:)                                    ! for particular solutions
+
     end if
 
     return
 contains
+    ! first-order derivative, explicit, 3. order
+    function coef_e1n3_biased(x, i, backwards) result(coef)
+        real(wp), intent(in) :: x(:)
+        integer(wi), intent(in) :: i
+        logical, intent(in), optional :: backwards
+        real(wp) coef(4)
+
+        integer(wi) stencil(4), k
+
+        if (present(backwards)) then
+            stencil = [i, i - 1, i - 2, i - 3]
+        else
+            stencil = [i, i + 1, i + 2, i + 3]
+        end if
+
+        do k = 1, size(stencil)
+            coef(k) = Lag_p(x, i, stencil(k), stencil(:))
+        end do
+        ! if uniform, [ -11/6 3 -3/2 1/3 ]/h
+
+        return
+    end function
+
+    ! first-order derivative, explicit, 2. order
+    function coef_e1n2_biased(x, i, backwards) result(coef)
+        real(wp), intent(in) :: x(:)
+        integer(wi), intent(in) :: i
+        logical, intent(in), optional :: backwards
+        real(wp) coef(3)
+
+        integer(wi) stencil(3), k
+
+        if (present(backwards)) then
+            stencil = [i, i - 1, i - 2]
+        else
+            stencil = [i, i + 1, i + 2]
+        end if
+
+        do k = 1, size(stencil)
+            coef(k) = Lag_p(x, i, stencil(k), stencil(:))
+        end do
+        ! if uniform, [ -3/2 2 -1/2 ]/h
+
+        return
+    end function
+
     function Lag_p(x, j, i, idx) result(f)                     ! 1. derivative of Lagrange polynomials on idx(:) around i
         use TLAB_CONSTANTS
         implicit none
