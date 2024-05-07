e12e8a0d9d747c42c691f820f7b5b306c280e870
Juan Pedro Mellado <mellado@gmx.de>
2024-05-07 12:16:08 +0200

    Redfining tmp arrays to retain upward flux in the average files.

--- src/mappings/avg_scal_xz.f90
+++ src/mappings/avg_scal_xz.f90
@@ -488,9 +488,9 @@ subroutine AVG_SCAL_XZ(is, q, s, s_local, dsdx, dsdy, dsdz, tmp1, tmp2, tmp3, me
 
     if (transport%active(is)) then      ! Transport in tmp3 and dsdz
         if (imode_eqns == DNS_EQNS_ANELASTIC) then
-            call THERMO_ANELASTIC_WEIGHT_OUTPLACE(imax, jmax, kmax, rbackground, s(:, :, :, transport%scalar(is)), tmp2)
-            call FI_TRANSPORT(transport, 1, imax, jmax, kmax, is, s, tmp2, tmp3, dsdy)
-            call FI_TRANSPORT_FLUX(transport, imax, jmax, kmax, is, s, tmp2, dsdz)
+            call THERMO_ANELASTIC_WEIGHT_OUTPLACE(imax, jmax, kmax, rbackground, s(:, :, :, transport%scalar(is)), dsdy)
+            call FI_TRANSPORT(transport, 1, imax, jmax, kmax, is, s, dsdy, tmp3, dsdz)
+            call FI_TRANSPORT_FLUX(transport, imax, jmax, kmax, is, s, dsdy, dsdz)
             call THERMO_ANELASTIC_WEIGHT_INPLACE(imax, jmax, kmax, ribackground, tmp3)
 
         else
