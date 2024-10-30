7a5ccfc807433a27bed7c66b2bb37f86c9c6c15a
Juan Pedro Mellado <mellado@gmx.de>
2024-10-30 17:19:30 +0100

    Further cleaning of code to remove header file types.h

--- src/fdm/fdm_c1n6m_coeff.f90
+++ src/fdm/fdm_c1n6m_coeff.f90
@@ -9,51 +9,51 @@
 !#
 !########################################################################
 !# DESCRIPTION
-!# 
+!#
 !# Coefficients of pentadiagonal 6th-order scheme for 1st derivative.
 !# WARNING: duplicated code, cf. coeff in FDM_C1N6_Jacobian_Penta!
-!# 
-!########################################################################
-!# ARGUMENTS 
 !#
 !########################################################################
-#include "types.h"
+!# ARGUMENTS
+!#
+!########################################################################
 
-SUBROUTINE FDM_C1N6M_COEFF()
+subroutine FDM_C1N6M_COEFF()
+    use TLab_Constants, only: wp, wi
 
-  USE FDM_PROCS, ONLY : C1N6M_ALPHA, C1N6M_BETA
-  USE FDM_PROCS, ONLY : C1N6M_ALPHA2, C1N6M_BETA2
-  USE FDM_PROCS, ONLY : C1N6M_A,   C1N6M_B,   C1N6M_C
-  USE FDM_PROCS, ONLY : C1N6M_AD2, C1N6M_BD4, C1N6M_CD6
-  USE FDM_PROCS, ONLY :            C1N6M_BD2, C1N6M_CD3
+    use FDM_PROCS, only: C1N6M_ALPHA, C1N6M_BETA
+    use FDM_PROCS, only: C1N6M_ALPHA2, C1N6M_BETA2
+    use FDM_PROCS, only: C1N6M_A, C1N6M_B, C1N6M_C
+    use FDM_PROCS, only: C1N6M_AD2, C1N6M_BD4, C1N6M_CD6
+    use FDM_PROCS, only: C1N6M_BD2, C1N6M_CD3
 
-  IMPLICIT NONE
+    implicit none
 
 ! #######################################################################
 ! -------------------------------------------------------------------
-! Pentadiagonal 6th-order scheme (JCP Lele 1992, Eq. 2.1.10) with 
+! Pentadiagonal 6th-order scheme (JCP Lele 1992, Eq. 2.1.10) with
 ! similar truncation error as 6th-order tridiagonal scheme
-! (JCP Lele 1992, Eq. 2.1.7 with alpha=1/3). Value of beta is derived 
-! with table 1 (2.1.7 = 2.1.10). Largest stable value with alpha=0.56. 
+! (JCP Lele 1992, Eq. 2.1.7 with alpha=1/3). Value of beta is derived
+! with table 1 (2.1.7 = 2.1.10). Largest stable value with alpha=0.56.
 ! (Simulations are unstable for larger alpha values!)
 ! (values of alpha and beta can be changed, according to Lele...)
 ! -------------------------------------------------------------------
-  C1N6M_ALPHA  = C_56_R/C_100_R
-  C1N6M_BETA   = (C_2_R/C_5_R )*(-C_1_R/C_3_R +        C1N6M_ALPHA                    )
-  C1N6M_A      = (C_1_R/C_6_R )*( C_9_R       +        C1N6M_ALPHA - C_20_R*C1N6M_BETA)
-  C1N6M_B      = (C_1_R/C_15_R)*(-C_9_R       + C_32_R*C1N6M_ALPHA + C_62_R*C1N6M_BETA)
-  C1N6M_C      = (C_1_R/C_10_R)*( C_1_R       - C_3_R *C1N6M_ALPHA + C_12_R*C1N6M_BETA)
+    C1N6M_ALPHA = 56.0_wp/100.0_wp
+    C1N6M_BETA = (2.0_wp/5.0_wp)*(-1.0_wp/3.0_wp + C1N6M_ALPHA)
+    C1N6M_A = (1.0_wp/6.0_wp)*(9.0_wp + C1N6M_ALPHA - 20.0_wp*C1N6M_BETA)
+    C1N6M_B = (1.0_wp/15.0_wp)*(-9.0_wp + 32.0_wp*C1N6M_ALPHA + 62.0_wp*C1N6M_BETA)
+    C1N6M_C = (1.0_wp/10.0_wp)*(1.0_wp - 3.0_wp*C1N6M_ALPHA + 12.0_wp*C1N6M_BETA)
 
 ! #######################################################################
-  C1N6M_AD2    = C1N6M_A / C_2_R 
-  C1N6M_BD4    = C1N6M_B / C_4_R
-  C1N6M_CD6    = C1N6M_C / C_6_R
-  !  
-  C1N6M_BD2    = C1N6M_B / C_2_R
-  C1N6M_CD3    = C1N6M_C / C_3_R
-  !
-  C1N6M_ALPHA2 = C_2_R * C1N6M_ALPHA
-  C1N6M_BETA2  = C_2_R * C1N6M_BETA
+    C1N6M_AD2 = C1N6M_A/2.0_wp
+    C1N6M_BD4 = C1N6M_B/4.0_wp
+    C1N6M_CD6 = C1N6M_C/6.0_wp
+    !
+    C1N6M_BD2 = C1N6M_B/2.0_wp
+    C1N6M_CD3 = C1N6M_C/3.0_wp
+    !
+    C1N6M_ALPHA2 = 2.0_wp*C1N6M_ALPHA
+    C1N6M_BETA2 = 2.0_wp*C1N6M_BETA
 
-  RETURN
-END SUBROUTINE FDM_C1N6M_COEFF
+    return
+end subroutine FDM_C1N6M_COEFF
--- src/fdm/int_c1n6m.f90
+++ src/fdm/int_c1n6m.f90
@@ -22,19 +22,19 @@
 !#     Au' = Bu                 N   eqns
 !#
 !# The system of N-1 eqns:
-!# 
+!#
 !#                    (B + \lambda A)u = Ah = l
 !#
 !# is established in this routine, giving diagonals a-g and l (see notes).
 !#
-!# Carpenter, Gottlieb and Aberbanel, JCP, 1993, study the effect of 
-!# boundary points on stability. Scheme 3-5-2xtri6c--penta6c--2xtri6c-5-3 
+!# Carpenter, Gottlieb and Aberbanel, JCP, 1993, study the effect of
+!# boundary points on stability. Scheme 3-5-2xtri6c--penta6c--2xtri6c-5-3
 !# is implmented.
 !#
 !# Solution array does not appear in this routine.
 !#
 !########################################################################
-!# ARGUMENTS 
+!# ARGUMENTS
 !#
 !# jkmax       In    number of systems to solve
 !# ibc         In    BCs: 1 u_1 given
@@ -43,303 +43,304 @@
 !# h           Out   forcing term for the exponential
 !#
 !########################################################################
-#include "types.h"
 
 !########################################################################
 !Left-hand side; heptadiagonal matrix of the linear system and h
 !########################################################################
-SUBROUTINE INT_C1N6M_LHS_E(imax, ibc, dx, lambda, a,b,c,d,e,f,g, h)
+subroutine INT_C1N6M_LHS_E(imax, ibc, dx, lambda, a, b, c, d, e, f, g, h)
+    use TLab_Constants, only: wp, wi
+    use FDM_PROCS, only: C1N6M_ALPHA, C1N6M_BETA
+    use FDM_PROCS, only: C1N6M_AD2, C1N6M_BD4, C1N6M_CD6
 
-  USE FDM_PROCS, ONLY : C1N6M_ALPHA, C1N6M_BETA
-  USE FDM_PROCS, ONLY : C1N6M_AD2, C1N6M_BD4, C1N6M_CD6
+    implicit none
 
-  IMPLICIT NONE
-
-  TREAL lambda
-  TINTEGER,               INTENT(IN)  :: imax, ibc
-  TREAL, DIMENSION(imax), INTENT(IN)  :: dx
-  TREAL, DIMENSION(imax), INTENT(OUT) :: a,b,c,d,e,f,g,h
+    real(wp) lambda
+    integer(wi), intent(IN) :: imax, ibc
+    real(wp), dimension(imax), intent(IN) :: dx
+    real(wp), dimension(imax), intent(OUT) :: a, b, c, d, e, f, g, h
 
 ! -------------------------------------------------------------------
-  TINTEGER                            :: i
-  TREAL                               :: c0136, c1418, c0103, c0104
+    integer(wi) :: i
+    real(wp) :: c0136, c1418, c0103, c0104
 
 ! ###################################################################
-  c0136 = C_1_R /C_36_R
-  c1418 = C_14_R/C_18_R
-  c0103 = C_1_R /C_3_R 
-  c0104 = C_1_R /C_4_R 
+    c0136 = 1.0_wp/36.0_wp
+    c1418 = 14.0_wp/18.0_wp
+    c0103 = 1.0_wp/3.0_wp
+    c0104 = 1.0_wp/4.0_wp
 
 ! -------------------------------------------------------------------
 ! Define diagonals of pentadiagonal system
 ! -------------------------------------------------------------------
 ! third-order biased
-  a(1     ) =   C_0_R 
-  b(1     ) =   C_0_R 
-  c(1     ) =   C_0_R 
-  d(1     ) = - C_5_R/C_2_R + lambda       *dx(1)
-  e(1     ) =   C_2_R       + lambda*C_2_R *dx(2)
-  f(1     ) =   C_05_R
-  g(1     ) =   C_0_R
+    a(1) = 0.0_wp
+    b(1) = 0.0_wp
+    c(1) = 0.0_wp
+    d(1) = -5.0_wp/2.0_wp + lambda*dx(1)
+    e(1) = 2.0_wp + lambda*2.0_wp*dx(2)
+    f(1) = 0.5_wp
+    g(1) = 0.0_wp
 ! fifth-order biased
-  a(2     ) =   C_0_R 
-  b(2     ) =   C_0_R 
-  c(2     ) = - C_5_R/C_9_R + lambda*C_1_R/C_6_R *dx(1)
-  d(2     ) = - C_1_R/C_2_R + lambda             *dx(2)
-  e(2     ) =   C_1_R       + lambda*C_1_R/C_2_R *dx(3)
-  f(2     ) =   C_1_R/C_18_R 
-  g(2     ) =   C_0_R 
+    a(2) = 0.0_wp
+    b(2) = 0.0_wp
+    c(2) = -5.0_wp/9.0_wp + lambda*1.0_wp/6.0_wp*dx(1)
+    d(2) = -1.0_wp/2.0_wp + lambda*dx(2)
+    e(2) = 1.0_wp + lambda*1.0_wp/2.0_wp*dx(3)
+    f(2) = 1.0_wp/18.0_wp
+    g(2) = 0.0_wp
 ! sixth-order centered (alpha=1/3)
-  DO i = 3,4 ! DO i = 3,3
-    a(i   ) =   C_0_R
-    b(i   ) = - c0136
-    c(i   ) = - c1418       + lambda*c0103  *dx(i-1)
-    d(i   ) =   C_0_R       + lambda        *dx(i  )
-    e(i   ) =   c1418       + lambda*c0103  *dx(i+1)
-    f(i   ) =   c0136
-    g(i   ) =   C_0_R
-  ENDDO
+    do i = 3, 4 ! DO i = 3,3
+        a(i) = 0.0_wp
+        b(i) = -c0136
+        c(i) = -c1418 + lambda*c0103*dx(i - 1)
+        d(i) = 0.0_wp + lambda*dx(i)
+        e(i) = c1418 + lambda*c0103*dx(i + 1)
+        f(i) = c0136
+        g(i) = 0.0_wp
+    end do
 ! sixth-order modified centered
-  DO i = 5,imax-4 ! DO i = 4,imax-3
-    a(i   ) = - C1N6M_CD6
-    b(i   ) = - C1N6M_BD4   + lambda* C1N6M_BETA  *dx(i-2)
-    c(i   ) = - C1N6M_AD2   + lambda* C1N6M_ALPHA *dx(i-1)
-    d(i   ) =   C_0_R       + lambda              *dx(i  )
-    e(i   ) =   C1N6M_AD2   + lambda* C1N6M_ALPHA *dx(i+1)
-    f(i   ) =   C1N6M_BD4   + lambda* C1N6M_BETA  *dx(i+2)
-    g(i   ) =   C1N6M_CD6
-  ENDDO
+    do i = 5, imax - 4 ! DO i = 4,imax-3
+        a(i) = -C1N6M_CD6
+        b(i) = -C1N6M_BD4 + lambda*C1N6M_BETA*dx(i - 2)
+        c(i) = -C1N6M_AD2 + lambda*C1N6M_ALPHA*dx(i - 1)
+        d(i) = 0.0_wp + lambda*dx(i)
+        e(i) = C1N6M_AD2 + lambda*C1N6M_ALPHA*dx(i + 1)
+        f(i) = C1N6M_BD4 + lambda*C1N6M_BETA*dx(i + 2)
+        g(i) = C1N6M_CD6
+    end do
 ! sixth-order centered (alpha=1/3)
-  DO i = imax-3,imax-2 ! DO i = imax-2,imax-2
-    a(i   ) =   C_0_R
-    b(i   ) = - c0136
-    c(i   ) = - c1418       + lambda*c0103  *dx(i-1)
-    d(i   ) =   C_0_R       + lambda        *dx(i  )
-    e(i   ) =   c1418       + lambda*c0103  *dx(i+1)
-    f(i   ) =   c0136
-    g(i   ) =   C_0_R
-  ENDDO
+    do i = imax - 3, imax - 2 ! DO i = imax-2,imax-2
+        a(i) = 0.0_wp
+        b(i) = -c0136
+        c(i) = -c1418 + lambda*c0103*dx(i - 1)
+        d(i) = 0.0_wp + lambda*dx(i)
+        e(i) = c1418 + lambda*c0103*dx(i + 1)
+        f(i) = c0136
+        g(i) = 0.0_wp
+    end do
 ! fifth-order biased
-  a(imax-1) =   C_0_R
-  b(imax-1) = - C_1_R/C_18_R
-  c(imax-1) = - C_1_R       + lambda*C_1_R/C_2_R *dx(imax-2)
-  d(imax-1) =   C_1_R/C_2_R + lambda             *dx(imax-1)
-  e(imax-1) =   C_5_R/C_9_R + lambda*C_1_R/C_6_R *dx(imax  )
-  f(imax-1) =   C_0_R 
-  g(imax-1) =   C_0_R 
+    a(imax - 1) = 0.0_wp
+    b(imax - 1) = -1.0_wp/18.0_wp
+    c(imax - 1) = -1.0_wp + lambda*1.0_wp/2.0_wp*dx(imax - 2)
+    d(imax - 1) = 1.0_wp/2.0_wp + lambda*dx(imax - 1)
+    e(imax - 1) = 5.0_wp/9.0_wp + lambda*1.0_wp/6.0_wp*dx(imax)
+    f(imax - 1) = 0.0_wp
+    g(imax - 1) = 0.0_wp
 ! third-order biased
-  a(imax  ) =   C_0_R      
-  b(imax  ) = - C_05_R      
-  c(imax  ) = - C_2_R       + lambda*C_2_R *dx(imax-1)
-  d(imax  ) =   C_5_R/C_2_R + lambda       *dx(imax  )
-  e(imax  ) =   C_0_R 
-  f(imax  ) =   C_0_R 
-  g(imax  ) =   C_0_R 
-    
+    a(imax) = 0.0_wp
+    b(imax) = -0.5_wp
+    c(imax) = -2.0_wp + lambda*2.0_wp*dx(imax - 1)
+    d(imax) = 5.0_wp/2.0_wp + lambda*dx(imax)
+    e(imax) = 0.0_wp
+    f(imax) = 0.0_wp
+    g(imax) = 0.0_wp
+
 ! -------------------------------------------------------------------
 ! Boundary conditions, see notes
 ! -------------------------------------------------------------------
-  IF      ( ibc .EQ. 1 ) THEN
-    d(1     ) = - C_5_R/C_2_R                                   ! array B22R
-    d(2     ) = - C_5_R /C_6_R  + lambda*C_2_R/C_3_R*dx(2)      ! B22R + lambda A22R fifth-order biased
-    e(2     ) =   C_11_R/C_12_R + lambda*C_1_R/C_2_R*dx(3)     
-    f(imax  ) =   C_1_R/C_6_R                                   ! element A(imax-1,imax)
+    if (ibc == 1) then
+        d(1) = -5.0_wp/2.0_wp                                   ! array B22R
+        d(2) = -5.0_wp/6.0_wp + lambda*2.0_wp/3.0_wp*dx(2)      ! B22R + lambda A22R fifth-order biased
+        e(2) = 11.0_wp/12.0_wp + lambda*1.0_wp/2.0_wp*dx(3)
+        f(imax) = 1.0_wp/6.0_wp                                   ! element A(imax-1,imax)
 
-  ELSE IF ( ibc .EQ. 2 ) THEN
-    b(1     ) =   C_1_R/C_6_R                                   ! element A(2,1)
-    c(imax-1) = - C_11_R/C_12_R + lambda*C_1_R/C_2_R*dx(imax-2) ! B11R + lambda A11R fifth-order biased
-    d(imax-1) =   C_5_R /C_6_R  + lambda*C_2_R/C_3_R*dx(imax-1)
-    d(imax  ) =   C_5_R/C_2_R                                   ! array B11R
+    else if (ibc == 2) then
+        b(1) = 1.0_wp/6.0_wp                                   ! element A(2,1)
+        c(imax - 1) = -11.0_wp/12.0_wp + lambda*1.0_wp/2.0_wp*dx(imax - 2) ! B11R + lambda A11R fifth-order biased
+        d(imax - 1) = 5.0_wp/6.0_wp + lambda*2.0_wp/3.0_wp*dx(imax - 1)
+        d(imax) = 5.0_wp/2.0_wp                                   ! array B11R
 
-  ENDIF
+    end if
 
 ! -------------------------------------------------------------------
 ! Setting the RHS for the null space (with minus sign)
 ! -------------------------------------------------------------------
-  h = C_0_R
-  IF      ( ibc .EQ. 1 ) THEN
-    h(1     ) =   C_1_R       ! normalization
-    h(2     ) =   c0136*C_5_R ! fifth-order biased
-    h(3     ) =   c0136
-  ELSE IF ( ibc .EQ. 2 ) THEN
-    h(imax-2) = - c0136       ! fifth-order biased
-    h(imax-1) = - c0136*C_5_R
-    h(imax  ) =   C_1_R       ! normalization
-  ENDIF
+    h = 0.0_wp
+    if (ibc == 1) then
+        h(1) = 1.0_wp       ! normalization
+        h(2) = c0136*5.0_wp ! fifth-order biased
+        h(3) = c0136
+    else if (ibc == 2) then
+        h(imax - 2) = -c0136       ! fifth-order biased
+        h(imax - 1) = -c0136*5.0_wp
+        h(imax) = 1.0_wp       ! normalization
+    end if
 
-  RETURN
-END SUBROUTINE INT_C1N6M_LHS_E
+    return
+end subroutine INT_C1N6M_LHS_E
 
 !########################################################################
 !Left-hand side case \lambda=0; pentadiagonal matrix of the linear system
 !########################################################################
-SUBROUTINE INT_C1N6M_LHS(imax, ibc, a,b,c,d,e,f,g)
+subroutine INT_C1N6M_LHS(imax, ibc, a, b, c, d, e, f, g)
+    use TLab_Constants, only: wp, wi
 
-  USE FDM_PROCS, ONLY : C1N6M_AD2, C1N6M_BD4, C1N6M_CD6
+    use FDM_PROCS, only: C1N6M_AD2, C1N6M_BD4, C1N6M_CD6
 
-  IMPLICIT NONE
+    implicit none
 
-  TINTEGER,               INTENT(IN) :: imax, ibc
-  TREAL, DIMENSION(imax), INTENT(OUT):: a,b,c,d,e,f,g
+    integer(wi), intent(IN) :: imax, ibc
+    real(wp), dimension(imax), intent(OUT) :: a, b, c, d, e, f, g
 
 ! -------------------------------------------------------------------
-  TREAL                              :: c0136, c1418
-  TINTEGER                           :: i
+    real(wp) :: c0136, c1418
+    integer(wi) :: i
 
 ! ###################################################################
-  c0136 = C_1_R /C_36_R
-  c1418 = C_14_R/C_18_R
+    c0136 = 1.0_wp/36.0_wp
+    c1418 = 14.0_wp/18.0_wp
 
 ! -------------------------------------------------------------------
 ! Define diagonals of heptadiagonal system
 ! -------------------------------------------------------------------
 ! third-order biased
-  a(1       ) =   C_0_R 
-  b(1       ) =   C_0_R 
-  c(1       ) =   C_0_R 
-  d(1       ) = - C_5_R/C_2_R
-  e(1       ) =   C_2_R
-  f(1       ) =   C_05_R      
-  g(1       ) =   C_0_R      
+    a(1) = 0.0_wp
+    b(1) = 0.0_wp
+    c(1) = 0.0_wp
+    d(1) = -5.0_wp/2.0_wp
+    e(1) = 2.0_wp
+    f(1) = 0.5_wp
+    g(1) = 0.0_wp
 ! fifth-order biased
-  a(2       ) =   C_0_R 
-  b(2       ) =   C_0_R 
-  c(2       ) = - C_5_R/C_9_R
-  d(2       ) = - C_1_R/C_2_R
-  e(2       ) =   C_1_R
-  f(2       ) =   C_1_R/C_18_R
-  g(2       ) =   C_0_R
+    a(2) = 0.0_wp
+    b(2) = 0.0_wp
+    c(2) = -5.0_wp/9.0_wp
+    d(2) = -1.0_wp/2.0_wp
+    e(2) = 1.0_wp
+    f(2) = 1.0_wp/18.0_wp
+    g(2) = 0.0_wp
 ! sixth-order centered (alpha=1/3)
-  DO i = 3,4 ! DO i = 3,3
-    a(i     ) =   C_0_R
-    b(i     ) = - c0136
-    c(i     ) = - c1418
-    d(i     ) =   C_0_R
-    e(i     ) =   c1418
-    f(i     ) =   c0136
-    g(i     ) =   C_0_R
-  ENDDO
+    do i = 3, 4 ! DO i = 3,3
+        a(i) = 0.0_wp
+        b(i) = -c0136
+        c(i) = -c1418
+        d(i) = 0.0_wp
+        e(i) = c1418
+        f(i) = c0136
+        g(i) = 0.0_wp
+    end do
 ! sixth-order modified centered
-  DO i = 5,imax-4 ! DO i = 4,imax-3
-    a(i     ) = - C1N6M_CD6
-    b(i     ) = - C1N6M_BD4
-    c(i     ) = - C1N6M_AD2
-    d(i     ) =   C_0_R
-    e(i     ) =   C1N6M_AD2
-    f(i     ) =   C1N6M_BD4
-    g(i     ) =   C1N6M_CD6
-  ENDDO
+    do i = 5, imax - 4 ! DO i = 4,imax-3
+        a(i) = -C1N6M_CD6
+        b(i) = -C1N6M_BD4
+        c(i) = -C1N6M_AD2
+        d(i) = 0.0_wp
+        e(i) = C1N6M_AD2
+        f(i) = C1N6M_BD4
+        g(i) = C1N6M_CD6
+    end do
 ! sixth-order centered (alpha=1/3)
-  DO i = imax-3,imax-2 ! DO i = imax-2,imax-2
-    a(i     ) =   C_0_R
-    b(i     ) = - c0136
-    c(i     ) = - c1418
-    d(i     ) =   C_0_R
-    e(i     ) =   c1418
-    f(i     ) =   c0136
-    g(i     ) =   C_0_R
-  ENDDO
+    do i = imax - 3, imax - 2 ! DO i = imax-2,imax-2
+        a(i) = 0.0_wp
+        b(i) = -c0136
+        c(i) = -c1418
+        d(i) = 0.0_wp
+        e(i) = c1418
+        f(i) = c0136
+        g(i) = 0.0_wp
+    end do
 ! fifth-order biased
-  a(  imax-1) =   C_0_R
-  b(  imax-1) = - C_1_R/C_18_R
-  c(  imax-1) = - C_1_R
-  d(  imax-1) =   C_1_R/C_2_R      
-  e(  imax-1) =   C_5_R/C_9_R
-  f(  imax-1) =   C_0_R 
-  g(  imax-1) =   C_0_R 
+    a(imax - 1) = 0.0_wp
+    b(imax - 1) = -1.0_wp/18.0_wp
+    c(imax - 1) = -1.0_wp
+    d(imax - 1) = 1.0_wp/2.0_wp
+    e(imax - 1) = 5.0_wp/9.0_wp
+    f(imax - 1) = 0.0_wp
+    g(imax - 1) = 0.0_wp
 ! third-order biased
-  a(  imax  ) =   C_0_R      
-  b(  imax  ) = - C_05_R      
-  c(  imax  ) = - C_2_R       
-  d(  imax  ) =   C_5_R/C_2_R 
-  e(  imax  ) =   C_0_R 
-  f(  imax  ) =   C_0_R 
-  g(  imax  ) =   C_0_R 
-     
+    a(imax) = 0.0_wp
+    b(imax) = -0.5_wp
+    c(imax) = -2.0_wp
+    d(imax) = 5.0_wp/2.0_wp
+    e(imax) = 0.0_wp
+    f(imax) = 0.0_wp
+    g(imax) = 0.0_wp
+
 ! -------------------------------------------------------------------
 ! Boundary conditions, see notes
 ! -------------------------------------------------------------------
-  IF      ( ibc .EQ. 1 ) THEN ! array B22R
-    d(2     ) = - C_5_R /C_6_R  ! fifth-order biased
-    e(2     ) =   C_11_R/C_12_R
-    f(imax  ) =   C_1_R/C_6_R
-         
-  ELSE IF ( ibc .EQ. 2 ) THEN ! array B11R
-    c(imax-1) = - C_11_R/C_12_R ! fifth-order biased
-    d(imax-1) =   C_5_R /C_6_R
-    b(1     ) =   C_1_R/C_6_R
-    
-  ENDIF
+    if (ibc == 1) then ! array B22R
+        d(2) = -5.0_wp/6.0_wp  ! fifth-order biased
+        e(2) = 11.0_wp/12.0_wp
+        f(imax) = 1.0_wp/6.0_wp
 
-  RETURN
-END SUBROUTINE INT_C1N6M_LHS
+    else if (ibc == 2) then ! array B11R
+        c(imax - 1) = -11.0_wp/12.0_wp ! fifth-order biased
+        d(imax - 1) = 5.0_wp/6.0_wp
+        b(1) = 1.0_wp/6.0_wp
+
+    end if
+
+    return
+end subroutine INT_C1N6M_LHS
 
 ! #######################################################################
 ! Right-hand side; forcing term
 ! #######################################################################
-SUBROUTINE INT_C1N6M_RHS(imax,jkmax, ibc, dx, h,l)
+subroutine INT_C1N6M_RHS(imax, jkmax, ibc, dx, h, l)
+    use TLab_Constants, only: wp, wi
 
-  USE FDM_PROCS, ONLY : C1N6M_ALPHA, C1N6M_BETA
+    use FDM_PROCS, only: C1N6M_ALPHA, C1N6M_BETA
 
-  IMPLICIT NONE
+    implicit none
 
-  TINTEGER,                    INTENT(IN) :: imax, jkmax, ibc
-  TREAL, DIMENSION(imax),      INTENT(IN) :: dx
-  TREAL, DIMENSION(jkmax,imax),INTENT(IN) :: h
-  TREAL, DIMENSION(jkmax,imax),INTENT(OUT):: l
+    integer(wi), intent(IN) :: imax, jkmax, ibc
+    real(wp), dimension(imax), intent(IN) :: dx
+    real(wp), dimension(jkmax, imax), intent(IN) :: h
+    real(wp), dimension(jkmax, imax), intent(OUT) :: l
 
 ! -------------------------------------------------------------------
-  TINTEGER                                :: i
-  TREAL                                   :: c0102, c0103, c0104, c0106, c0203
+    integer(wi) :: i
+    real(wp) :: c0102, c0103, c0104, c0106, c0203
 
 ! ###################################################################
-  c0102 = C_1_R /C_2_R 
-  c0103 = C_1_R /C_3_R 
-  c0104 = C_1_R /C_4_R 
-  c0106 = C_1_R /C_6_R 
-  c0203 = C_2_R /C_3_R 
+    c0102 = 1.0_wp/2.0_wp
+    c0103 = 1.0_wp/3.0_wp
+    c0104 = 1.0_wp/4.0_wp
+    c0106 = 1.0_wp/6.0_wp
+    c0203 = 2.0_wp/3.0_wp
 
 ! -------------------------------------------------------------------
 ! Boundary conditions
 ! -------------------------------------------------------------------
-  IF      ( ibc .EQ. 1 ) THEN ! array A22R
+    if (ibc == 1) then ! array A22R
 ! BCs, see notes
-    l(:,1     ) = - C_2_R*h(:,2)*dx(2)                      ! contribution to u'_1
-    l(:,2     ) =   c0102*h(:,3)*dx(3) + c0203*h(:,2)*dx(2) ! fifth-order biased
+        l(:, 1) = -2.0_wp*h(:, 2)*dx(2)                      ! contribution to u'_1
+        l(:, 2) = c0102*h(:, 3)*dx(3) + c0203*h(:, 2)*dx(2) ! fifth-order biased
 ! fifth-order biased
-    l(:,imax-1) =   c0102*h(:,imax-2)*dx(imax-2)+h(:,imax-1)*dx(imax-1)+c0106*h(:,imax)*dx(imax)
+        l(:, imax - 1) = c0102*h(:, imax - 2)*dx(imax - 2) + h(:, imax - 1)*dx(imax - 1) + c0106*h(:, imax)*dx(imax)
 ! third-order biased
-    l(:,imax  ) =   h(:,imax)*dx(imax  ) + C_2_R*h(:,imax-1)*dx(imax-1)
+        l(:, imax) = h(:, imax)*dx(imax) + 2.0_wp*h(:, imax - 1)*dx(imax - 1)
 
 ! -------------------------------------------------------------------
-  ELSE IF ( ibc .EQ. 2 ) THEN ! array A11R
+    else if (ibc == 2) then ! array A11R
 ! third-order biased
-    l(:,1     ) =   h(:,1)*dx(1) + C_2_R*h(:,2)*dx(2)
+        l(:, 1) = h(:, 1)*dx(1) + 2.0_wp*h(:, 2)*dx(2)
 ! fifth-order
-    l(:,2     ) =   c0106*h(:,1)*dx(1)+h(:,2)*dx(2)+c0102*h(:,3)*dx(3)          ! fifth-order biased
+        l(:, 2) = c0106*h(:, 1)*dx(1) + h(:, 2)*dx(2) + c0102*h(:, 3)*dx(3)          ! fifth-order biased
 ! BCs, see notes
-    l(:,imax-1) =   c0102*h(:,imax-2)*dx(imax-2) + c0203*h(:,imax-1)*dx(imax-1) ! fifth-order biased
-    l(:,imax  ) = - C_2_R*h(:,imax-1)*dx(imax-1)                                ! contribution to u'_N
-  ENDIF
+        l(:, imax - 1) = c0102*h(:, imax - 2)*dx(imax - 2) + c0203*h(:, imax - 1)*dx(imax - 1) ! fifth-order biased
+        l(:, imax) = -2.0_wp*h(:, imax - 1)*dx(imax - 1)                                ! contribution to u'_N
+    end if
 
 ! -------------------------------------------------------------------
 ! Interior points
 ! -------------------------------------------------------------------
 ! 6th-order centered with alpha=(1/3)
-  DO i = 3,4 ! DO i = 3,3
-  l(:,i       ) =   h(:,i)*dx(i) + c0103*( h(:,i-1)*dx(i-1)  + h(:,i+1)*dx(i+1) ) 
-  ENDDO
-  !
-  DO i = imax-3,imax-2 ! DO i = imax-2,imax-2
-  l(:,i       ) =   h(:,i)*dx(i) + c0103*( h(:,i-1)*dx(i-1)  + h(:,i+1)*dx(i+1) ) 
-  ENDDO
+    do i = 3, 4 ! DO i = 3,3
+        l(:, i) = h(:, i)*dx(i) + c0103*(h(:, i - 1)*dx(i - 1) + h(:, i + 1)*dx(i + 1))
+    end do
+    !
+    do i = imax - 3, imax - 2 ! DO i = imax-2,imax-2
+        l(:, i) = h(:, i)*dx(i) + c0103*(h(:, i - 1)*dx(i - 1) + h(:, i + 1)*dx(i + 1))
+    end do
 
 ! sixth-order modified centered
-  DO i = 5,imax-4 ! DO i = 4,imax-3
-    l(:,i     ) =                   h(:,i  )*dx(i  )                       + &
-                    C1N6M_ALPHA * ( h(:,i-1)*dx(i-1)  + h(:,i+1)*dx(i+1) ) + &
-                    C1N6M_BETA  * ( h(:,i-2)*dx(i-2)  + h(:,i+2)*dx(i+2) )
-  ENDDO
+    do i = 5, imax - 4 ! DO i = 4,imax-3
+        l(:, i) = h(:, i)*dx(i) + &
+                  C1N6M_ALPHA*(h(:, i - 1)*dx(i - 1) + h(:, i + 1)*dx(i + 1)) + &
+                  C1N6M_BETA*(h(:, i - 2)*dx(i - 2) + h(:, i + 2)*dx(i + 2))
+    end do
 
-  RETURN
-END SUBROUTINE INT_C1N6M_RHS
+    return
+end subroutine INT_C1N6M_RHS
