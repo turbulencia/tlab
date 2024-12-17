130f60425c6821153d7d9be43469464c8441d281
Juan Pedro Mellado <mellado@gmx.de>
2024-12-17 09:21:38 +0100

    Debuging mpi_io.

--- src/valid/mpi/vmpi_io.f90
+++ src/valid/mpi/vmpi_io.f90
@@ -1,224 +1,105 @@
-!# Compile with
-!# mpxlf90_r -d -qsuffix=cpp=f90 -WF,-DUSE_MPI,-DUSE_MPI_IO -q64 -qextname -qsave -qarch=pwr6 -qtune=pwr6 -O3 -qenablevmx -qhot=simd -qessl -o vmpi_io.x vmpi_io.f90
-#define SIZEOFREAL 8
-#define SIZEOFINT  4
+! debug code for levante where we are experiencing some problems possibly due to stripping of disks.
+program vmpi_io_levante
+    use MPI
+    implicit none
 
-#define TREAL      REAL(8)
-#define TINTEGER   INTEGER(4)
+    integer mpio_fh, mpio_locsize, status(MPI_STATUS_SIZE), ims_Err
+    integer(KIND=MPI_OFFSET_KIND) mpio_disp
+    integer subarray
 
-!########################################################################
-MODULE DNS_MPI
-  IMPLICIT NONE
-  SAVE
+    integer, parameter :: sp = kind(1.0)
+    integer, parameter :: dp = kind(1.0d0)
+    integer, parameter :: wp = dp               ! working precision
+    integer, parameter :: wi = selected_int_kind(9)
 
-  INTEGER  :: ims_pro,  ims_pro_i,  ims_pro_j,  ims_pro_k
-  INTEGER  :: ims_npro, ims_npro_i, ims_npro_j, ims_npro_k
-  INTEGER  :: ims_err
+    ! using 1536 cores, 12 nodes. Decomposition along X and Z in 48x32 pencils
+    integer ims_npro                            ! number of tasks
+    ! integer, parameter :: ims_npro_i = 48       ! number of tasks in X
+    ! integer, parameter :: ims_npro_k = 32       ! number of tasks in Z
+    integer, parameter :: ims_npro_i = 2       ! number of tasks in X
+    integer, parameter :: ims_npro_k = 2       ! number of tasks in Z
+    integer ims_pro                             ! local task in global communicator
+    integer ims_pro_i, ims_pro_k                ! local task in each directional communicator; here used only as offsets
 
-END MODULE DNS_MPI
+    ! grid points
+    integer nx, ny, nz
 
-!########################################################################
-MODULE DNS_GLOBAL
-  IMPLICIT NONE
-  SAVE
+    ! number of variables
+    integer, parameter :: nv = 3
+    integer iv
 
-  TINTEGER :: imax_total, jmax_total, kmax_total
+    ! array
+    real(sp), allocatable :: a(:, :)
+    ! real(dp), allocatable :: a(:, :)
 
-END MODULE DNS_GLOBAL
+    ! file name
+    character(*), parameter :: name = 'test-io.'
+    character(len=32) str
 
-!########################################################################
-PROGRAM VMPI_IO
+    ! ###############################################################
+    call MPI_INIT(ims_err)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, ims_npro, ims_err)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, ims_pro, ims_err)
 
-  USE TLabMPI_VARS
-  USE TLAB_VARS
+    ! initialize grid
+    ny = 384
+    ! nx = 1536
+    ! nz = 1536
+    nx = 16
+    nz = 24
+    nx = nx/ims_npro_i                      ! task-local number of grid points along X
+    nz = nz/ims_npro_k                      ! task-local number of grid points along Z
+    
+    ims_pro_i = mod(ims_pro, ims_npro_i)    ! MPI offset
+    ims_pro_k = ims_pro/ims_npro_i          ! MPI offset
 
-  IMPLICIT NONE
+    allocate (a(nx*ny*nz, nv))
+    a = 0.0_wp
 
-#ifdef USE_MPI
-#include "mpif.h"
-#endif
+    ! single precission
+    subarray = IO_CREATE_SUBARRAY_XOZ(nx, ny, nz, MPI_REAL4)
+    ! double precission
+    ! subarray = IO_CREATE_SUBARRAY_XOZ(nx, ny, nz, MPI_REAL8)
 
-  TINTEGER imax, jmax, kmax
-  TINTEGER inb_flow, inb_flow_array, inb_scal, inb_scal_array
-  TINTEGER ip, it, it_max
+    mpio_disp = 0
+    mpio_locsize = nx*ny*nz
+    do iv = 1, nv
+        call MPI_BARRIER(MPI_COMM_WORLD, ims_err)
 
-  TREAL, DIMENSION(:,:), ALLOCATABLE, SAVE :: q, s
-  TREAL, DIMENSION(:),   ALLOCATABLE, SAVE :: wrk3d
+        write (str, *) iv; str = trim(adjustl(name))//trim(adjustl(str))
+        call MPI_FILE_OPEN(MPI_COMM_WORLD, str, MPI_MODE_WRONLY+MPI_MODE_CREATE, MPI_INFO_NULL, mpio_fh, ims_err)
 
-  CHARACTER*32 :: name_loc
-  TINTEGER ifield
+        ! single precission
+        call MPI_File_set_view(mpio_fh, mpio_disp, MPI_REAL4, subarray, 'native', MPI_INFO_NULL, ims_err)
+        call MPI_File_write_all(mpio_fh, a(:, iv), mpio_locsize, MPI_REAL4, status, ims_err)
+        ! double precission
+        ! call MPI_File_set_view(mpio_fh, mpio_disp, MPI_REAL8, subarray, 'native', MPI_INFO_NULL, ims_err)
+        ! call MPI_File_write_all(mpio_fh, a(:, :, :, iv), mpio_locsize, MPI_REAL8, status, ims_err)
 
-  TINTEGER isize_max, isize
-  PARAMETER(isize_max=20)
-  TREAL params(isize_max)
+        call MPI_File_close(mpio_fh, ims_err)
+    end do
 
-  TINTEGER :: i1=1, i2=2
+    call MPI_FINALIZE(ims_err)
 
-! ###################################################################
-  imax_total = 1024
-  jmax_total =  512
-  kmax_total = 1024
+    stop
 
-  imax       = 1024; ims_npro_i = imax_total/imax
-  jmax       =  512
-  kmax       =    8; ims_npro_k = kmax_total/kmax
+contains
+    function IO_CREATE_SUBARRAY_XOZ(nx, ny, nz, mpi_type) result(subarray)
+        integer(wi), intent(in) :: nx, ny, nz
+        integer, intent(in) :: mpi_type
 
-  inb_flow       = 3
-  inb_flow_array = inb_flow
-  inb_scal       = 1
-  inb_scal_array = inb_scal
+        integer :: subarray
+        integer, parameter :: ndims = 3
+        integer(wi) :: sizes(ndims), locsize(ndims), offset(ndims)
 
-  ALLOCATE(    q(imax*jmax*kmax, inb_flow_array))
-  ALLOCATE(    s(imax*jmax*kmax, inb_scal_array))
-  ALLOCATE(wrk3d(imax*jmax*kmax)                )
+        sizes = [nx*ims_npro_i, ny, nz*ims_npro_k]
+        locsize = [nx, ny, nz]
+        offset = [nx*ims_pro_i, 0, nz*ims_pro_k]
 
-! ###################################################################
-! from DNS_START
-  call MPI_INIT(ims_err)
-  call MPI_COMM_SIZE(MPI_COMM_WORLD,ims_npro,ims_err)
-  call MPI_COMM_RANK(MPI_COMM_WORLD,ims_pro, ims_err)
+        call MPI_Type_create_subarray(ndims, sizes, locsize, offset, &
+                                      MPI_ORDER_FORTRAN, mpi_type, subarray, ims_err)
+        call MPI_Type_commit(subarray, ims_err)
 
-! ###################################################################
-! from TLabMPI_Initialize()
-  ims_pro_i = MOD(ims_pro,ims_npro_i) ! Starting at 0
-  ims_pro_k =     ims_pro/ims_npro_i  ! Starting at 0
+    end function IO_CREATE_SUBARRAY_XOZ
 
-! ###################################################################
-  it_max = 20
-
-  DO it = 0,it_max
-     IF ( ims_pro .EQ. 0 ) THEN
-        OPEN(UNIT=22, FILE='dns.out', STATUS='unknown',POSITION='APPEND')
-        WRITE(22,*) 'Iteration ...:', it
-        CLOSE(22)
-     ENDIF
-
-! velocity
-     DO ifield = 1,3
-        WRITE(name_loc,'(I2)') ifield
-        name_loc='flow.'//TRIM(ADJUSTL(name_loc))
-        isize = isize_max
-        CALL IO_READ_FIELDS_SPLIT(name_loc, i2, imax,jmax,kmax,it, isize,params, q(1,ifield),wrk3d)
-     ENDDO
-
-! scalar
-     DO ifield = 1,1
-        WRITE(name_loc,'(I2)') ifield
-        name_loc='scal.'//TRIM(ADJUSTL(name_loc))
-        isize = isize_max
-        CALL IO_READ_FIELDS_SPLIT(name_loc, i1, imax,jmax,kmax,it, isize,params, s(1,ifield),wrk3d)
-     ENDDO
-
-  ENDDO
-
-  CALL MPI_FINALIZE(ims_err)
-
-  STOP
-END PROGRAM VMPI_IO
-
-!########################################################################
-#define LOC_UNIT_ID 54
-#define LOC_STATUS 'old'
-#define SIZEOFBYTE 1
-
-SUBROUTINE IO_READ_FIELDS_SPLIT(name, iheader, nx,ny,nz,nt, isize,params, a, wrk)
-
-  USE TLAB_VARS,ONLY : imax_total,jmax_total,kmax_total
-#ifdef USE_MPI
-  USE TLabMPI_VARS
-#endif
-
-  IMPLICIT NONE
-
-#ifdef USE_MPI
-#include "mpif.h"
-#endif
-
-  CHARACTER*(*) name
-  TINTEGER iheader, nx,ny,nz,nt, isize
-  TREAL, DIMENSION(isize)            :: params
-  TREAL, DIMENSION(nx*ny*nz), TARGET :: a, wrk
-
-! -------------------------------------------------------------------
-  TINTEGER header_offset
-
-#ifdef USE_MPI
-#ifdef USE_MPI_IO
-  INTEGER mpio_fh, mpio_locsize, status(MPI_STATUS_SIZE)
-  INTEGER(KIND=MPI_OFFSET_KIND) mpio_disp, mpio_locoff
-  TINTEGER id
-#endif
-
-#endif
-
-#ifdef USE_MPI
-#ifdef USE_MPI_IO
-! ###################################################################
-! Use MPI_IO for restart files
-! ###################################################################
-! -------------------------------------------------------------------
-! header
-! -------------------------------------------------------------------
-  IF ( iheader .GT. 0 ) THEN
-     IF ( ims_pro .EQ. 0 ) THEN
-        OPEN(LOC_UNIT_ID,file=name,status=LOC_STATUS,form='unformatted',access='stream')
-        REWIND(LOC_UNIT_ID)
-        CALL IO_READ_HEADER(LOC_UNIT_ID, header_offset, imax_total,jmax_total,kmax_total,nt, params)
-        CLOSE(LOC_UNIT_ID)
-     ENDIF
-     CALL MPI_BCAST(header_offset, 1, MPI_INTEGER4, 0, MPI_COMM_WORLD, ims_err)
-
-! Displacement to start of field
-     mpio_disp = header_offset*SIZEOFBYTE
-
-! Size of array params
-     isize = (header_offset - 5*SIZEOFINT)/SIZEOFREAL
-     CALL MPI_BCAST(params, isize, MPI_REAL8, 0, MPI_COMM_WORLD, ims_err)
-
-  ELSE
-     mpio_disp = 0
-
-  ENDIF
-
-! -------------------------------------------------------------------
-! fields
-! -------------------------------------------------------------------
-  CALL MPI_FILE_OPEN(MPI_COMM_WORLD, name, MPI_MODE_RDONLY, MPI_INFO_NULL, mpio_fh, ims_err)
-
-  mpio_locsize = nx*ny*nz
-  mpio_locoff  = mpio_locsize         ! mpio_locoff might be of type larger than INT4
-  mpio_locoff  = ims_pro*mpio_locoff  ! mpio_locoff might be of type larger than INT4
-  CALL MPI_FILE_SET_VIEW(mpio_fh, mpio_disp, MPI_REAL8, MPI_REAL8, 'native', MPI_INFO_NULL, ims_err)
-  CALL MPI_FILE_READ_AT_ALL(mpio_fh, mpio_locoff, a, mpio_locsize, MPI_REAL8, status, ims_err)
-
-  CALL MPI_FILE_CLOSE(mpio_fh, ims_err)
-
-#endif
-#endif
-
-  RETURN
-END SUBROUTINE IO_READ_FIELDS_SPLIT
-
-#undef LOC_UNIT_ID
-#undef LOC_STATUS
-
-!########################################################################
-SUBROUTINE IO_READ_HEADER(unit, offset, nx,ny,nz,nt, params)
-
-  IMPLICIT NONE
-
-  TINTEGER unit, offset, nx,ny,nz,nt
-  TREAL, DIMENSION(*) :: params
-
-! -------------------------------------------------------------------
-  TINTEGER isize, nx_loc, ny_loc, nz_loc, nt_loc
-
-!########################################################################
-  READ(unit) offset, nx_loc, ny_loc, nz_loc, nt_loc
-
-  isize = offset - 5*SIZEOFINT
-  isize = isize/SIZEOFREAL
-  READ(unit) params(1:isize)
-
-  RETURN
-END SUBROUTINE IO_READ_HEADER
+end program
